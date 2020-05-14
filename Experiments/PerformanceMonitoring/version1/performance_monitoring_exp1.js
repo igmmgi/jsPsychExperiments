0aa66c38b6d02b9af940476b94545e322e028718
ian <ian.mackenzie@uni-tuebingen.de>
2020-05-14 13:17:50 +0200

    shapes + keys

--- Experiments/PerformanceMonitoring/version1/performance_monitoring_exp1.js
+++ Experiments/PerformanceMonitoring/version1/performance_monitoring_exp1.js
@@ -11,6 +11,34 @@ const canvas_colour = "rgba(200, 200, 200, 1)";
 const canvas_size = [960, 720];
 const canvas_border = "5px solid black";
 
+////////////////////////////////////////////////////////////////////////
+//                             Experiment                             //
+////////////////////////////////////////////////////////////////////////
+const expName = getFileName();
+const dirName = getDirName();
+const vpNum = genVpNum();
+
+////////////////////////////////////////////////////////////////////////
+//                           Exp Parameters                           //
+////////////////////////////////////////////////////////////////////////
+const prms = {
+    nTrlsP:  20, 
+    nTrlsE: 100,
+    nBlks: 5, 
+    fixDur: 500,
+    fbDur: 500,
+    waitDur: 1000,
+    tooFast:  250,   
+    tooSlow: 1500,   
+    fbTxt: ["Richtig", "Falsch", "Zu langsam", "Zu schnell"],
+    respKeys: ["S", "D", "K", "L"],
+    respShapes: shuffle(["square", "circle", "triangle", "star"]),
+    shapeSize: 50,
+    cTrl: 1,
+    cBlk: 1,
+    respMapping: jsPsych.randomization.sampleWithoutReplacement([1, 2], 1)[0],
+}
+
 
 ////////////////////////////////////////////////////////////////////////
 //                              Stimuli                               //
@@ -44,39 +72,44 @@ function drawShape() {
     let ctx = document.getElementById('canvas').getContext('2d');
 
     ctx.fillStyle = "black"
-    
-    // example square
     ctx.translate(-300, 0);
-    ctx.fillRect(-50, -50, 100, 100)
+    ctx.font = "50px monospace";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
 
-    // example circle
-    ctx.translate(200, 0);
-    ctx.arc(0, 0, 50, 0, Math.PI * 2);
-    ctx.fill();
-
-    // example triangle
-    ctx.translate(200, 0);
-    ctx.beginPath();
-    ctx.moveTo(0, -50);
-    ctx.lineTo(-50, 50);
-    ctx.lineTo( 50, 50);
-    ctx.fill();
-
-    // example star shape
-    ctx.translate(200, 0);
-    let points = 5;
-    let size = 60;
-    let inset = 0.5;
-    ctx.beginPath();
-    ctx.moveTo(0, -size);
-    for (var i = 0; i < points; i++) {
-        ctx.rotate(Math.PI / points);
-        ctx.lineTo(0, 0 - (size*inset));
-        ctx.rotate(Math.PI / points);
-        ctx.lineTo(0, -size);
+    for (let i = 0; i < 4; i++) {
+        if (prms.respShapes[i] == "square") {
+            ctx.fillRect(-prms.shapeSize, -prms.shapeSize, prms.shapeSize*2, prms.shapeSize*2)
+            ctx.fillText(prms.respKeys[i], 0, prms.shapeSize * 1.5); 
+        } else if (prms.respShapes[i] == "circle") {
+            ctx.beginPath();
+            ctx.arc(0, 0, prms.shapeSize, 0, Math.PI * 2);
+            ctx.fill();
+            ctx.fillText(prms.respKeys[i], 0, prms.shapeSize * 1.5); 
+        } else if (prms.respShapes[i] == "triangle") {
+            ctx.beginPath();
+            ctx.moveTo(0, -prms.shapeSize);
+            ctx.lineTo(-prms.shapeSize, prms.shapeSize);
+            ctx.lineTo( prms.shapeSize, prms.shapeSize);
+            ctx.fill();
+            ctx.fillText(prms.respKeys[i], 0, prms.shapeSize * 1.5); 
+        } else if (prms.respShapes[i] == "star") {
+            let points = 5;
+            let size = prms.shapeSize * 1.2;
+            let inset = 0.5;
+            ctx.beginPath();
+            ctx.moveTo(0, -size);
+            for (let i = 0; i < points; i++) {
+                ctx.rotate(Math.PI / points);
+                ctx.lineTo(0, 0 - (size*inset));
+                ctx.rotate(Math.PI / points);
+                ctx.lineTo(0, -size);
+            }
+            ctx.fill();
+            ctx.fillText(prms.respKeys[i], 0, prms.shapeSize * 1.5); 
+        }
+        ctx.translate(200, 0);
     }
-    ctx.fill();
-
 
     ctx.restore();
 
@@ -88,20 +121,13 @@ const shape_stimulus = {
     canvas_colour: canvas_colour,
     canvas_size: canvas_size,
     canvas_border: canvas_border,
-    trial_duration: 5000,
+    trial_duration: 15000,
     translate_origin: true,
     stimulus_onset: 0,
     response_ends_trial: true,
     func: [drawShape]
 };
 
-
-////////////////////////////////////////////////////////////////////////
-//                             Experiment                             //
-////////////////////////////////////////////////////////////////////////
-const expName = getFileName();
-const dirName = getDirName();
-const vpNum = genVpNum();
 
 const fullscreen_on = {
     type: 'fullscreen',
