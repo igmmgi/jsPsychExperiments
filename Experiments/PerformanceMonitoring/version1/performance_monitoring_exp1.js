5868dc1f87f4a89f37ac121bfe99cbb082ad3861
ian <ian.mackenzie@uni-tuebingen.de>
2020-05-19 14:47:17 +0200

    full screen

--- Experiments/PerformanceMonitoring/version1/performance_monitoring_exp1.js
+++ Experiments/PerformanceMonitoring/version1/performance_monitoring_exp1.js
@@ -358,9 +358,9 @@ function genExpSeq() {
 
     let exp = [];
     
-    // exp.push(fullscreen_on);
-    // exp.push(welcome_de);
-    // exp.push(vpInfoForm_de);
+    exp.push(fullscreen_on);
+    exp.push(welcome_de);
+    exp.push(vpInfoForm_de);
     exp.push(task_instructions1);
 
     // random order following initial partial/full practice blocks
