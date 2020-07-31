c971807353e28471776d06665b5d8ffe18e863bf
ian <ian.mackenzie@uni-tuebingen.de>
2020-07-31 10:51:24 +0200

    ButtonNegation update

--- Experiments/ButtonNegation/version3/ButtonNegationV3.js
+++ Experiments/ButtonNegation/version3/ButtonNegationV3.js
@@ -660,7 +660,7 @@ function genExpSeq() {
     
     exp.push(fullscreen_on);
     exp.push(welcome_de);
-    // exp.push(vpInfoForm_de);
+    exp.push(vpInfoForm_de);
     exp.push(hideMouseCursor);
     exp.push(screenInfo);
     exp.push(task_instructions1);
