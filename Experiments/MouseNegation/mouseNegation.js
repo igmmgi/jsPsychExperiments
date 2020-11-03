0b4b34c5d3d34a6c83d83ded9617b6671b5964f6
igm <ian.mackenzie@uni-tuebingen.de>
2020-11-03 20:48:15 +0100

    updated mouseNegation

--- Experiments/MouseNegation/mouseNegation.js
+++ Experiments/MouseNegation/mouseNegation.js
@@ -202,7 +202,7 @@ function genExpSeq() {
   exp.push(fullscreen_on);
   exp.push(welcome_de);
   exp.push(resize_de);
-  exp.push(vpInfoForm_de);
+  // exp.push(vpInfoForm_de);
   exp.push(task_instructions);
 
   for (let blk = 0; blk < prms.nBlks; blk += 1) {
