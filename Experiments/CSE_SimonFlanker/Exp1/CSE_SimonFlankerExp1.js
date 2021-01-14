131cee76912c00bd62ad022aa8cb935615c866c5
ian <ian.mackenzie@uni-tuebingen.de>
2021-01-14 07:40:08 +0100

    formatting

--- Experiments/CSE_SimonFlanker/Exp1/CSE_SimonFlankerExp1.js
+++ Experiments/CSE_SimonFlanker/Exp1/CSE_SimonFlankerExp1.js
@@ -2,7 +2,7 @@
 // Standard visual Simon with left/right lateralized stimuli
 // Standard visual flanker task with two trial types:
 // 1) Central target and flankers presented simultaneously
-// 2) Central target presented 300 ms after presentation of flankers 
+// 2) Central target presented 300 ms after presentation of flankers
 
 ////////////////////////////////////////////////////////////////////////
 const cc = 'rgba(200, 200, 200, 1)';
@@ -48,132 +48,126 @@ jsPsych.data.addProperties({ version: nVersion });
 ////////////////////////////////////////////////////////////////////////
 
 // response key mapping for baseline simon
-const respText = 
-    generate_formatted_html({
-        text:  prms.respLetters[0] + " = linker Zeigefinger (Taste 'Q')",
-        bold: true,
-        fontsize: 26}) +
-    generate_formatted_html({
-        text:  prms.respLetters[1] + " = rechter Zeigefinger (Taste 'P')",
-        bold: true,
-        fontsize: 26})
-
+const respText =
+  generate_formatted_html({
+    text: prms.respLetters[0] + " = linker Zeigefinger (Taste 'Q')",
+    bold: true,
+    fontsize: 26,
+  }) +
+  generate_formatted_html({
+    text: prms.respLetters[1] + " = rechter Zeigefinger (Taste 'P')",
+    bold: true,
+    fontsize: 26,
+  });
 
 const task_instructions1 = {
-    type: 'html-keyboard-response-canvas',
-    canvas_colour: cc,
-    canvas_size: cs,
-    canvas_border: cb,
-    stimulus: 
-    generate_formatted_html({
-        text: `Willkommen bei unserem Experiment:<br><br> 
+  type: 'html-keyboard-response-canvas',
+  canvas_colour: cc,
+  canvas_size: cs,
+  canvas_border: cb,
+  stimulus: generate_formatted_html({
+    text: `Willkommen bei unserem Experiment:<br><br> 
         Die Teilnahme ist freiwillig und du darfst das Experiment jederzeit abbrechen. 
         Bitte stelle sicher, dass du dich in einer ruhigen Umgebung befindest und 
         genügend Zeit hast, um das Experiment durchzuführen. 
         Wir bitten dich die ca. 40 Minuten konzentriert zu arbeiten.<br><br>
         Drücke eine beliebige Taste, um fortzufahren!`,
-        bold: true,
-        fontsize: 26,
-        align: "left",
-        lineheight: 1.5 
-    })
+    bold: true,
+    fontsize: 26,
+    align: 'left',
+    lineheight: 1.5,
+  }),
 };
 
-
 const task_instructions2 = {
-    type: 'html-keyboard-response-canvas',
-    canvas_colour: cc,
-    canvas_size: cs,
-    canvas_border: cb,
-    stimulus:
-    generate_formatted_html({
-        text: `Du erhaelst den Code für die Versuchspersonenstunden und weitere Anweisungen
+  type: 'html-keyboard-response-canvas',
+  canvas_colour: cc,
+  canvas_size: cs,
+  canvas_border: cb,
+  stimulus: generate_formatted_html({
+    text: `Du erhaelst den Code für die Versuchspersonenstunden und weitere Anweisungen
         am Ende des Experimentes. Bei Fragen oder Problemen wende dich bitte an:<br><br>
         hiwipibio@gmail.com<br><br>
         Drücke eine beliebige Taste, um fortzufahren!`,
-        bold: true,
-        fontsize: 26,
-        align: "left",
-        lineheight: 1.5 
-    })
+    bold: true,
+    fontsize: 26,
+    align: 'left',
+    lineheight: 1.5,
+  }),
 };
 
 const task_instructions3 = {
-    type: 'html-keyboard-response-canvas',
-    canvas_colour: cc,
-    canvas_size: cs,
-    canvas_border: cb,
-    stimulus:
+  type: 'html-keyboard-response-canvas',
+  canvas_colour: cc,
+  canvas_size: cs,
+  canvas_border: cb,
+  stimulus:
     generate_formatted_html({
-        text: `Aufgabe:<br><br>
+      text: `Aufgabe:<br><br>
         In diesem Experiment musst du auf verschiedene Buchstaben
         so schnell und so genau wie möglich reagieren. Der Ziel-Buchstabe
         erscheint in manchen Durchgängen in der Mitte des Bildschirms (und ist
         von irrelevanten Buchstaben umgeben) und in anderen Durchgängen links oder
         rechts auf dem Bildschirm. Es gilt die folgende Zuordnung:<br><br>`,
-        bold: true,
-        fontsize: 26,
-        align: "left",
-        lineheight: 1.5 
+      bold: true,
+      fontsize: 26,
+      align: 'left',
+      lineheight: 1.5,
     }) +
     respText +
     generate_formatted_html({
-        text: `Drücke eine beliebige Taste, um fortzufahren!`,
-        bold: true,
-        fontsize: 26,
-        align: "left",
-        lineheight: 1.5 
-    }) 
+      text: `Drücke eine beliebige Taste, um fortzufahren!`,
+      bold: true,
+      fontsize: 26,
+      align: 'left',
+      lineheight: 1.5,
+    }),
 };
 
-
 const task_instructions4 = {
-    type: 'html-keyboard-response-canvas',
-    canvas_colour: cc,
-    canvas_size: cs,
-    canvas_border: cb,
-    stimulus:
-    generate_formatted_html({
-        text: `Reagiere immer nur auf den Ziel-Buchstaben. Das heißt:<br><br>
+  type: 'html-keyboard-response-canvas',
+  canvas_colour: cc,
+  canvas_size: cs,
+  canvas_border: cb,
+  stimulus: generate_formatted_html({
+    text: `Reagiere immer nur auf den Ziel-Buchstaben. Das heißt:<br><br>
         Wenn der Ziel-Buchstabe in der Mitte erscheint, dann ignoriere die umliegenden Buchstaben.<br><br>
         Wenn nur ein Ziel-Buchstabe links oder rechts auf dem Bildschirm präsentiert wird,
         dann ignoriere die links/rechts Position auf dem Bildschirm.<br><br>
         Reagiere so schnell und so genau wie möglich!<br><br>
         Drücke eine beliebige Taste, um fortzufahren.`,
-        bold: true,
-        fontsize: 26,
-        align: "left",
-        lineheight: 1.5 
-    }) 
+    bold: true,
+    fontsize: 26,
+    align: 'left',
+    lineheight: 1.5,
+  }),
 };
 
-
 const task_instructions_block = {
-    type: 'html-keyboard-response-canvas',
-    canvas_colour: cc,
-    canvas_size: cs,
-    canvas_border: cb,
-    stimulus:
+  type: 'html-keyboard-response-canvas',
+  canvas_colour: cc,
+  canvas_size: cs,
+  canvas_border: cb,
+  stimulus:
     generate_formatted_html({
-        text: `Wenn du bereit für den Block bist, dann positioniere
+      text: `Wenn du bereit für den Block bist, dann positioniere
         deine Hände auf die Tastatur.<br> 
         Zeil - Buchstabe erscheint entweder in der Mitte des Bildschirms
         oder links/rechts auf dem Bildschirm. Es gilt:`,
-        bold: true,
-        fontsize: 26,
-        align: "left",
-        lineheight: 1.5 
+      bold: true,
+      fontsize: 26,
+      align: 'left',
+      lineheight: 1.5,
     }) +
     respText +
     generate_formatted_html({
-        text: `Drücke eine beliebige Taste, um fortzufahren!`,
-        bold: true,
-        fontsize: 26,
-        align: "left",
-        lineheight: 1.5 
-    }) 
+      text: `Drücke eine beliebige Taste, um fortzufahren!`,
+      bold: true,
+      fontsize: 26,
+      align: 'left',
+      lineheight: 1.5,
+    }),
 };
-
 
 ////////////////////////////////////////////////////////////////////////
 //                              Stimuli                               //
@@ -202,16 +196,15 @@ const fixation_cross = {
 };
 
 function drawFeedback() {
-    'use strict';
-    let ctx = document.getElementById('canvas').getContext('2d');
-    let dat = jsPsych.data.get().last(1).values()[0];
-    ctx.font = prms.fbSize;
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillStyle = 'black';
-    ctx.fillText(prms.fbTxt[dat.corrCode], 0, 0);
+  'use strict';
+  let ctx = document.getElementById('canvas').getContext('2d');
+  let dat = jsPsych.data.get().last(1).values()[0];
+  ctx.font = prms.fbSize;
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = 'black';
+  ctx.fillText(prms.fbTxt[dat.corrCode], 0, 0);
 }
-
 
 function codeTrial() {
   'use strict';
@@ -279,11 +272,7 @@ const block_feedback = {
   },
 };
 
-
-
-
-
-const randomString = generateRandomStringWithExpName("CSE_SF1", 16);
+const randomString = generateRandomStringWithExpName('CSE_SF1', 16);
 
 const alphaNum = {
   type: 'html-keyboard-response-canvas',
@@ -296,7 +285,7 @@ const alphaNum = {
     "<h3 style='text-align:left;'>Wenn du eine Versuchspersonenstunde benötigst, </h3>" +
     "<h3 style='text-align:left;'>kopiere den folgenden zufällig generierten Code</h3>" +
     "<h3 style='text-align:left;'>und sende diesen zusammen mit deiner Matrikelnummer</h3><br>" +
-    "<h3 style='text-align:left;'>und deiner Universität (Bremen/Tübingen) per Email an:</h3><br>" +
+    "<h3 style='text-align:left;'>und deiner Universität per Email an:</h3><br>" +
     '<h2>hiwipibio@gmail.com</h2>' +
     '<h1>Code: ' +
     randomString +
@@ -325,7 +314,6 @@ const save_code = {
   timing_post_trial: 200,
 };
 
-
 ////////////////////////////////////////////////////////////////////////
 //                    Generate and run experiment                     //
 ////////////////////////////////////////////////////////////////////////
@@ -345,7 +333,6 @@ function genExpSeq() {
   exp.push(task_instructions3);
   exp.push(task_instructions4);
 
-
   // save data
   // exp.push(save_data);
   // exp.push(save_code);
@@ -361,9 +348,9 @@ function genExpSeq() {
 const EXP = genExpSeq();
 
 jsPsych.init({
-    timeline: EXP,
-    exclusions: {
-        min_width: cs[0],
-        min_height: cs[1],
-    },
+  timeline: EXP,
+  exclusions: {
+    min_width: cs[0],
+    min_height: cs[1],
+  },
 });
