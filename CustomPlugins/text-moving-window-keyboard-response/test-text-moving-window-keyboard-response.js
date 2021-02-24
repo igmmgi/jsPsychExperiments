6dc1b33bb23af287df369fb9ff09f6a948d93098
ian <ian.mackenzie@uni-tuebingen.de>
2021-02-24 17:49:12 +0100

    updated text-moving-window

--- CustomPlugins/text-moving-window-keyboard-response/test-text-moving-window-keyboard-response.js
+++ CustomPlugins/text-moving-window-keyboard-response/test-text-moving-window-keyboard-response.js
@@ -8,38 +8,49 @@ let timeline = [];
 
 // Mask type 1
 for (let sent_num = 0; sent_num < sentences.length; sent_num++) {
-  for (let word_num = -1; word_num < sentences[sent_num].split(' ').length; word_num++) {
-    let moving_window = {
-      type: 'text-moving-window-keyboard-response',
-      mask_type: 1,
-      sentence: sentences[sent_num],
-      word_number: word_num,
-      font: '24px monospace',
-      max_width: 960,
-      text_align: 'left',
-    };
-    timeline.push(moving_window);
-  }
+  let moving_window = {
+    type: 'text-moving-window-keyboard-response',
+    mask_type: 1,
+    sentence: sentences[sent_num],
+    font: '24px monospace',
+    x_align: 'left',
+    translate_origin: true,
+    xy_position: [-300, 0],
+  };
+  timeline.push(moving_window);
 }
 
 // Mask type 2
 for (let sent_num = 0; sent_num < sentences.length; sent_num++) {
-  for (let word_num = -1; word_num < sentences[sent_num].split(' ').length; word_num++) {
-    let moving_window = {
-      type: 'text-moving-window-keyboard-response',
-      mask_type: 2,
-      sentence: sentences[sent_num],
-      word_number: word_num,
-      font: '24px monospace',
-      max_width: 960,
-      text_align: 'center',
-    };
-    timeline.push(moving_window);
-  }
+  let moving_window = {
+    type: 'text-moving-window-keyboard-response',
+    mask_type: 2,
+    sentence: sentences[sent_num],
+    font: '24px monospace',
+    x_align: 'center',
+    translate_origin: true,
+    xy_position: [0, 0],
+  };
+  timeline.push(moving_window);
 }
+
+// // Mask type 3
+// for (let sent_num = 0; sent_num < sentences.length; sent_num++) {
+//   let moving_window = {
+//     type: 'text-moving-window-keyboard-response',
+//     mask_type: 3,
+//     sentence: sentences[sent_num],
+//     font: '24px monospace',
+//     x_align: 'center',
+//     translate_origin: true,
+//     xy_position: [0, 0],
+//   };
+//   timeline.push(moving_window);
+// }
 
 jsPsych.init({
   timeline: timeline,
+  default_iti: 500,
   on_finish: function () {
     jsPsych.data.displayData('csv');
   },
