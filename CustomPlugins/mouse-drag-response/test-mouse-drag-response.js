6d276c3fe3811a297d509f2de164f6cda9caac64
ian <ian.mackenzie@uni-tuebingen.de>
2021-03-09 11:49:25 +0100

    added test for mouse-drag-response

---
+++ CustomPlugins/mouse-drag-response/test-mouse-drag-response.js
@@ -0,0 +1,20 @@
+// jsPsych mouse-box-response
+
+let timeline = [];
+
+const trial_stimulus = {
+  type: 'mouse-drag-response',
+  canvas_size: [1280, 960],
+  canvas_colour: 'lightgrey',
+  canvas_border: '5px solid black',
+  response_border: [100, 860],
+};
+
+timeline.push(trial_stimulus);
+
+jsPsych.init({
+  timeline: timeline,
+  on_finish: function () {
+    jsPsych.data.displayData('json');
+  },
+});
