643503c7db3484b4df9bcc2e5ee897f5e38ee786
ian <ian.mackenzie@uni-tuebingen.de>
2021-06-25 10:54:01 +0200

    info form

--- Experiments/EmotionalMorality/Exp1/EmotionalMorality.js
+++ Experiments/EmotionalMorality/Exp1/EmotionalMorality.js
@@ -842,7 +842,7 @@ function genExpSeq() {
   exp.push(check_screen);
   exp.push(welcome_de_du);
   exp.push(resize_de_du);
-  // exp.push(vpInfoForm_de);
+  exp.push(vpInfoForm_de);
   exp.push(hideMouseCursor);
   exp.push(screenInfo);
 
