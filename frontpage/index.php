bba070528abb876bc5e6fed5a300e9b0b23e34e1
ian <ian.mackenzie@uni-tuebingen.de>
2022-09-08 10:29:28 +0200

    new experiment added

--- frontpage/index.php
+++ frontpage/index.php
@@ -27,9 +27,6 @@ $nFiles_ts4 = numFilesInDir('Experiments7+/TaskSwitching/VTS_TaskDifficulty/Exp1
 // flanker grouping
 $nFiles_fg = numFilesInDir('Experiments7+/flankerGrouping/Exp2/data');
 
-// Simon Emotion 
-$nFiles_se = numFilesInDir('Experiments7+/simonEmotion/Exp1.2/data');
-
 // Beats Exp1 
 $nFiles_beats_v1 = numFilesInDir('Experiments7+/ModalCongruency/Exp2/data/version1');
 $nFiles_beats_v2 = numFilesInDir('Experiments7+/ModalCongruency/Exp2/data/version2');
@@ -39,6 +36,26 @@ $nFiles_beats_v4 = numFilesInDir('Experiments7+/ModalCongruency/Exp2/data/versio
 // Treasure Hunt 
 $nFiles_th_v1 = numFilesInDir('Experiments/ProactiveReward/Exp2.3/data/version1');
 $nFiles_th_v2 = numFilesInDir('Experiments/ProactiveReward/Exp2.3/data/version2');
+
+// VTS_Effector Experiments 2 + 3
+$nFiles_vts_ef_exp2_v1 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp2/data/version1');
+$nFiles_vts_ef_exp2_v2 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp2/data/version2');
+$nFiles_vts_ef_exp2_v3 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp2/data/version3');
+$nFiles_vts_ef_exp2_v4 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp2/data/version4');
+$nFiles_vts_ef_exp2_v5 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp2/data/version5');
+$nFiles_vts_ef_exp2_v6 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp2/data/version6');
+$nFiles_vts_ef_exp2_v7 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp2/data/version7');
+$nFiles_vts_ef_exp2_v8 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp2/data/version8');
+
+$nFiles_vts_ef_exp3_v1 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp3/data/version1');
+$nFiles_vts_ef_exp3_v2 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp3/data/version2');
+$nFiles_vts_ef_exp3_v3 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp3/data/version3');
+$nFiles_vts_ef_exp3_v4 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp3/data/version4');
+$nFiles_vts_ef_exp3_v5 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp3/data/version5');
+$nFiles_vts_ef_exp3_v6 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp3/data/version6');
+$nFiles_vts_ef_exp3_v7 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp3/data/version7');
+$nFiles_vts_ef_exp3_v8 = numFilesInDir('Experiments7+/TaskSwitching/VTS_Effector/Exp3/data/version8');
+
 
 
 ?>
@@ -81,7 +98,8 @@ $nFiles_th_v2 = numFilesInDir('Experiments/ProactiveReward/Exp2.3/data/version2'
             <p>In der linken Spalte können Sie zwischen verschiedenen Experimenten wählen. Eine Teilnahme is ab 18 Jahren möglich. Bitte bearbeiten Sie das ausgewählte Experiment an einem ruhigen Ort und an einem Computer/Laptop (nicht am Handy/Tablet). Bitte bearbeiten Sie ein ausgewähltes Experiment nur einmal!</b>
             <p>Versuchspersonenstunden: Bei einigen Experimenten ist es möglich VP-Stunden zu erhalten. Sie erhalten Ihren VP-Code und weitere Anweisungen am Ende eines Experimentes. </p>
 
-            <p style="color:red;">***ACHTUNG: Für das Experiment "Beats Exp1" benötigen Sie Lautsprecher oder Kopfhörer.***</p>
+            <p style="color:red;">*** ACHTUNG: Für das Experiment "Beats Exp1" benötigen Sie Lautsprecher oder Kopfhörer. ***</p>
+            <p style="color:blue;">*** Für die Teilnahme am Experiment „Pick!“ können Sie entscheiden, ob Sie 1 VP-Stunde oder 10€ (per Überweisung) erhalten möchten. ***</p>
             
             <p>Die Teilnahme ist freiwillig. Die Daten werden anonym gespeichert und können nicht mit Ihnen in Verbindung gebracht werden. Basierend auf den Richtlinien guter ethischer Forschung sowie der Datenschutzgrundverordnung sollen sich Teilnehmende explizit und nachvollziehbar mit der Teilnahme an einem Experiment und der Verarbeitung ihrer Daten einverstanden erklären. Aus diesem Grund möchten wir Sie bitten, die "Allgemeinen Informationen für TeilnehmerInnen" aufmerksam zu lesen before Sie an einem Experiment teilnehmen.</p>
 
@@ -101,18 +119,86 @@ $nFiles_th_v2 = numFilesInDir('Experiments/ProactiveReward/Exp2.3/data/version2'
 
             <h1> Verantwortliche Ansprechpartner während der Studie </h1>
             <h2> Bei Fragen oder Problemen sprechen Sie bitte den verantwortlichen Ansprechpartner des Experimentes an.</h2>
+            <p> Pick!: hiwipibio@gmail.com </p>
             <p> This or That Exp2: Experiment.TaskSwitching@gmail.com </p>
             <p> Beats Exp1: hiwipibio@gmail.com </p>
             <p> Treasure Hunt: hiwipibio@gmail.com </p>
             <p> Dots and Dashes: hiwipibio@gmail.com </p>
             <p> Mouse Negation: m.zeller@student.uni-tuebingen.de </p>
-            <p> Faces Exp2: charlotte.kost@student.uni-tuebingen.de</p>
 
         </div>
         <div class="sidenav">
 
             <h1>Experiments</h1>
             <h2>1,0 VP-Stunden</h2>
+
+
+            <?php
+            $pick = [];
+
+            if ($nFiles_vts_ef_exp2_v1 < 1) {
+                array_push($pick, 1);
+            }
+            if ($nFiles_vts_ef_exp2_v2 < 1) {
+                array_push($pick, 2);
+            }
+            if ($nFiles_vts_ef_exp2_v3 < 1) {
+                array_push($pick, 3);
+            }
+            if ($nFiles_vts_ef_exp2_v4 < 1) {
+                array_push($pick, 4);
+            }
+            if ($nFiles_vts_ef_exp2_v5 < 1) {
+                array_push($pick, 5);
+            }
+            if ($nFiles_vts_ef_exp2_v6 < 1) {
+                array_push($pick, 6);
+            }
+            if ($nFiles_vts_ef_exp2_v7 < 1) {
+                array_push($pick, 7);
+            }
+            if ($nFiles_vts_ef_exp2_v8 < 1) {
+                array_push($pick, 8);
+            }
+            if ($nFiles_vts_ef_exp3_v1 < 1) {
+                array_push($pick, 9);
+            }
+            if ($nFiles_vts_ef_exp3_v2 < 1) {
+                array_push($pick, 10);
+            }
+            if ($nFiles_vts_ef_exp3_v3 < 1) {
+                array_push($pick, 11);
+            }
+            if ($nFiles_vts_ef_exp3_v4 < 1) {
+                array_push($pick, 12);
+            }
+            if ($nFiles_vts_ef_exp3_v5 < 1) {
+                array_push($pick, 13);
+            }
+            if ($nFiles_vts_ef_exp3_v6 < 1) {
+                array_push($pick, 14);
+            }
+            if ($nFiles_vts_ef_exp3_v7 < 1) {
+                array_push($pick, 15);
+            }
+            if ($nFiles_vts_ef_exp3_v8 < 1) {
+                array_push($pick, 16);
+            }
+
+            $randIndex = array_rand($pick);
+            $pick_version = $pick[$randIndex];
+
+            ?>
+
+
+            <?php if (!empty($pick_version) && ($pick_version <= 8)) : ?>
+                <h3><a href="Experiments7+/TaskSwitching/VTS_Effector/Exp2/index.html?version=<?php echo $pick_version; ?>">Pick! (n = <?= $nFiles_vts_ef_exp2_v1 + $nFiles_vts_ef_exp2_v2 + $nFiles_vts_ef_exp2_v3 + $nFiles_vts_ef_exp2_v4 + $nFiles_vts_ef_exp2_v5 + $nFiles_vts_ef_exp2_v6 + $nFiles_vts_ef_exp2_v7 + $nFiles_vts_ef_exp2_v8 + $nFiles_vts_ef_exp3_v1 + $nFiles_vts_ef_exp3_v2 + $nFiles_vts_ef_exp3_v3 + $nFiles_vts_ef_exp3_v4 + $nFiles_vts_ef_exp3_v5 + $nFiles_vts_ef_exp3_v6 + $nFiles_vts_ef_exp3_v7 + $nFiles_vts_ef_exp3_v8?>/16)</a></h3>
+            <?php endif;  ?>
+
+            <?php if (!empty($pick_version) && ($pick_version > 8)) : ?>
+                <h3><a href="Experiments7+/TaskSwitching/VTS_Effector/Exp3/index.html?version=<?php echo $pick_version-8; ?>">Pick! (n = <?= $nFiles_vts_ef_exp2_v1 + $nFiles_vts_ef_exp2_v2 + $nFiles_vts_ef_exp2_v3 + $nFiles_vts_ef_exp2_v4 + $nFiles_vts_ef_exp2_v5 + $nFiles_vts_ef_exp2_v6 + $nFiles_vts_ef_exp2_v7 + $nFiles_vts_ef_exp2_v8 + $nFiles_vts_ef_exp3_v1 + $nFiles_vts_ef_exp3_v2 + $nFiles_vts_ef_exp3_v3 + $nFiles_vts_ef_exp3_v4 + $nFiles_vts_ef_exp3_v5 + $nFiles_vts_ef_exp3_v6 + $nFiles_vts_ef_exp3_v7 + $nFiles_vts_ef_exp3_v8?>/16)</a></h3>
+            <?php endif;  ?>
+
 
             <?php
             $ts = [];
@@ -134,7 +220,7 @@ $nFiles_th_v2 = numFilesInDir('Experiments/ProactiveReward/Exp2.3/data/version2'
             ?>
 
             <?php if (!empty($ts_version)) : ?>
-                <h3><a href="Experiments7+/TaskSwitching/VTS_TaskDifficulty/Exp1/index.html?version=<?php echo $ts_version; ?>">This or That Exp2 (n = <?= $nFiles_ts1 + $nFiles_ts2 + $nFiles_ts3 + $nFiles_ts4 ?>)</a></h3>
+                <h3><a href="Experiments7+/TaskSwitching/VTS_TaskDifficulty/Exp1/index.html?version=<?php echo $ts_version; ?>">This or That Exp2 (n = <?= $nFiles_ts1 + $nFiles_ts2 + $nFiles_ts3 + $nFiles_ts4 ?>/50)</a></h3>
             <?php endif;  ?>
 
 
@@ -158,7 +244,7 @@ $nFiles_th_v2 = numFilesInDir('Experiments/ProactiveReward/Exp2.3/data/version2'
             ?>
 
             <?php if (!empty($beats_version)) : ?>
-                <h3><a href="Experiments7+/ModalCongruency/Exp2/index.html?version=<?php echo $beats_version; ?>">Beats Exp1 (n = <?= $nFiles_beats_v1 + $nFiles_beats_v2 + $nFiles_beats_v3 + $nFiles_beats_v4 ?>)</a></h3>
+                <h3><a href="Experiments7+/ModalCongruency/Exp2/index.html?version=<?php echo $beats_version; ?>">Beats Exp1 (n = <?= $nFiles_beats_v1 + $nFiles_beats_v2 + $nFiles_beats_v3 + $nFiles_beats_v4 ?>/40)</a></h3>
             <?php endif;  ?>
 
             <?php
@@ -175,7 +261,7 @@ $nFiles_th_v2 = numFilesInDir('Experiments/ProactiveReward/Exp2.3/data/version2'
             ?>
 
             <?php if (!empty($th_version)) : ?>
-                <h3><a href="Experiments/ProactiveReward/Exp2.3/index.html?orderVersion=<?php echo $th_version; ?>">Treasure Hunt (n = <?= $nFiles_th_v1 + $nFiles_th_v2 ?>)</a></h3>
+                <h3><a href="Experiments/ProactiveReward/Exp2.3/index.html?orderVersion=<?php echo $th_version; ?>">Treasure Hunt (n = <?= $nFiles_th_v1 + $nFiles_th_v2 ?>/40)</a></h3>
             <?php endif;  ?>
 
 
@@ -198,20 +284,16 @@ $nFiles_th_v2 = numFilesInDir('Experiments/ProactiveReward/Exp2.3/data/version2'
 
             <?php if (!empty($mn_version)) : ?>
                 <?php if ($mn_version == 1) : ?>
-                    <h3><a href="Experiments/MouseNegation/Exp2/index.html">Mouse Negation Exp2 (n = <?= $nFiles_mn2 + $nFiles_mn3 ?>)</a></h3>
+                    <h3><a href="Experiments/MouseNegation/Exp2/index.html">Mouse Negation Exp2 (n = <?= $nFiles_mn2 + $nFiles_mn3 ?>/200)</a></h3>
                 <?php endif;  ?>
                 <?php if ($mn_version == 2) : ?>
-                    <h3><a href="Experiments/MouseNegation/Exp3/index.html">Mouse Negation Exp2 (n = <?= $nFiles_mn2 + $nFiles_mn3 ?>)</a></h3>
+                    <h3><a href="Experiments/MouseNegation/Exp3/index.html">Mouse Negation Exp2 (n = <?= $nFiles_mn2 + $nFiles_mn3 ?>/200)</a></h3>
                 <?php endif;  ?>
             <?php endif;  ?>
 
 
    <?php if ($nFiles_fg < 40) : ?>
-                <h3><a href="Experiments7+/flankerGrouping/Exp2/index.html">Dots and Dashes Exp2 (n = <?= $nFiles_fg ?>)</a></h3>
-            <?php endif;  ?>
-
- <?php if ($nFiles_se < 50) : ?>
-                <h3><a href="Experiments7+/simonEmotion/Exp1.2/index.html">Faces Exp2 (n = <?= $nFiles_se ?>)</a></h3>
+                <h3><a href="Experiments7+/flankerGrouping/Exp2/index.html">Dots and Dashes Exp2 (n = <?= $nFiles_fg ?>/40)</a></h3>
             <?php endif;  ?>
 
 
