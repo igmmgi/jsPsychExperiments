b2dd079290892df682a3f8dd29c2f28959fd867f
ian <ian.mackenzie@uni-tuebingen.de>
2024-04-10 15:34:40 +0200

    pilot exp

---
+++ Experiments7+/LearningTransferEmotion/Exp1/images/Neutral/neutral.js
@@ -0,0 +1,10 @@
+const NEUTRAL_IMAGES = [
+    "images/Neutral/021_o_f_n_a.jpg",
+    "images/Neutral/033_o_m_n_b.jpg",
+    "images/Neutral/060_o_f_n_a.jpg",
+    "images/Neutral/091_o_m_n_b.jpg",
+    "images/Neutral/109_y_m_n_b.jpg",
+    "images/Neutral/125_y_f_n_b.jpg",
+    "images/Neutral/132_y_f_n_b.jpg",
+    "images/Neutral/144_y_m_n_a.jpg",
+];
