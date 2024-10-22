526d7bf9fd366a1d5c82386aa82afc4d35c2b77d
igmmgi <ian.mackenzie@uni-tuebingen.de>
2024-10-22 09:41:45 +0200

    updated pilot template

--- Experiments8+/GestureConflict/Exp1/GestureConflict.js
+++ Experiments8+/GestureConflict/Exp1/GestureConflict.js
@@ -27,26 +27,33 @@ const jsPsych = initJsPsych({
 });
 
 const CANVAS_COLOUR = "rgba(255, 255, 255, 1)";
-const CANVAS_SIZE = [1280, 720];
-const CANVAS_BORDER = "5px solid Black";
+const CANVAS_SIZE = [720, 1280];
 
 // Experiment Parameters
 const PRMS = {
-    ntrls: 64, // number of trials per block (multiple of 8)
-    nblks: 12, // number of blocks
+    ntrls: 8, // number of trials per block (multiple of 8)
+    nblks: 2, // number of blocks
     fix_size: 10, // size of the fixation cross
     fix_width: 5, // width of fixation cross
-    fix_duration: 500, // duration of the fixation cross
-    feedback_duration: [0, 1500, 1500, 1500], // feedback duration for response type (correct, incorrect, too slow, too fast)
-    too_slow: 2000, // feedback duration for correct and incorrect trials, respectively
+    fix_duration: 1000, // duration of the fixation cross
+    feedback_duration: [1000, 1000, 1000, 1000], // feedback duration for response type (correct, incorrect, too slow, too fast)
+    too_slow: 5000, // feedback duration for correct and incorrect trials, respectively
     too_fast: 0, // feedback duration for correct and incorrect trials, respectively
-    feedback_text: ["", "Falsch!", "Zu langsam!", "Zu schnell!"],
+    feedback_text: ["Richtig!", "Falsch!", "Zu langsam!", "Zu schnell!"],
     iti: 500, // duration of the inter-trial-interval
     feedback_font: "50px Arial",
-    resp_keys: ["S", "H"],
+    resp_keys: ["Q", "P"],
+    resp_mapping: shuffle(["Nein", "Ja"]), // no/yes randomly assigned to left right/keys
     ctrl: 1,
     cblk: 1,
 };
+
+// 4 versions
+// const VERSION = Number(jsPsych.data.urlVariables().version); // version is provided in the url
+// jsPsych.data.addProperties({ version: VERSION });
+
+// or set explicitly if testing
+const VERSION = 1;
 
 ////////////////////////////////////////////////////////////////////////
 //                      Experiment Instructions                       //
@@ -71,46 +78,56 @@ Drücke eine beliebige Taste, um fortzufahren`,
     post_trial_gap: 1000,
 };
 
-const TASK_INSTRUCTIONS1 = {
-    type: jsPsychHtmlKeyboardResponseCanvas,
-    canvas_colour: CANVAS_COLOUR,
+const TASK_INSTRUCTIONS = {
+    type: jsPsychHtmlKeyboardResponse,
     canvas_size: CANVAS_SIZE,
-    canvas_border: CANVAS_BORDER,
-    choices: [" "],
     stimulus: generate_formatted_html({
-        text: `Willkommen zum Experiment!<br><br>
-Bitte bearbeiten Sie das nachfolgende Experiment ernsthaft und konzentriert. Stellen Sie außerdem sicher, 
-dass Sie sich in einer ruhigen Umgebung befinden und über ausreichend Zeit verfügen.<br><br>
-Bei Fragen oder Problemen lassen Sie uns bitte eine Nachricht via Prolific zukommen.<br><br> 
-Drücken Sie die Leertaste, um fortzufahren.`,
+        text: `Do something ...<br><br>
+${PRMS.resp_keys[0]}-Taste = ${PRMS.resp_mapping[0]} &ensp;&ensp;&ensp;&ensp; ${PRMS.resp_keys[1]}-Taste = ${PRMS.resp_mapping[1]}<br><br>
+Drücke eine beliebige Taste, um fortzufahren`,
         align: "left",
-        colour: "Black",
-        fontsize: 28,
+        color: "black",
+        fontsize: 30,
+        bold: false,
     }),
+    post_trial_gap: 1000,
 };
 
 ////////////////////////////////////////////////////////////////////////
 //                              Stimuli                               //
 ////////////////////////////////////////////////////////////////////////
 
-const VIDEOS = [
-    "../videos/JaDaumenhoch_f.mp4",
-    "../videos/NeinDaumenhoch_f.mp4",
-    "../videos/JaDaumenhoch_m.mp4",
-    "../videos/NeinDaumenhoch_m.mp4",
-    "../videos/JaDaumenrunter_f.mp4",
-    "../videos/NeinDaumenrunter_f.mp4",
-    "../videos/JaDaumenrunter_m.mp4",
-    "../videos/NeinDaumenrunter_m.mp4",
-    "../videos/JaKopfJa_f.mp4",
-    "../videos/NeinKopfJa_f.mp4",
-    "../videos/JaKopfJa_m.mp4",
-    "../videos/NeinKopfJa_m.mp4",
-    "../videos/JaKopfNein_f.mp4",
-    "../videos/NeinKopfNein_f.mp4",
-    "../videos/JaKopfNein_m.mp4",
-    "../videos/NeinKopfNein_m.mp4",
-];
+function assign_video_files() {
+    "use strict";
+    let videos_head = [
+        "../Exp1/videos/JaKopfJa_f.mp4",
+        "../Exp1/videos/NeinKopfJa_f.mp4",
+        "../Exp1/videos/JaKopfJa_m.mp4",
+        "../Exp1/videos/NeinKopfJa_m.mp4",
+        "../Exp1/videos/JaKopfNein_f.mp4",
+        "../Exp1/videos/NeinKopfNein_f.mp4",
+        "../Exp1/videos/JaKopfNein_m.mp4",
+        "../Exp1/videos/NeinKopfNein_m.mp4",
+    ];
+    let videos_thumb = [
+        "../Exp1/videos/JaDaumenhoch_f.mp4",
+        "../Exp1/videos/NeinDaumenhoch_f.mp4",
+        "../Exp1/videos/JaDaumenhoch_m.mp4",
+        "../Exp1/videos/NeinDaumenhoch_m.mp4",
+        "../Exp1/videos/JaDaumenrunter_f.mp4",
+        "../Exp1/videos/NeinDaumenrunter_f.mp4",
+        "../Exp1/videos/JaDaumenrunter_m.mp4",
+        "../Exp1/videos/NeinDaumenrunter_m.mp4",
+    ];
+    if ([1, 2].includes(VERSION)) {
+        return videos_head;
+    } else if ([3, 4].includes(VERSION)) {
+        return videos_thumb;
+    }
+}
+
+const VIDEOS = assign_video_files();
+// console.log(VIDEOS);
 
 const PRELOAD_VIDEOS = {
     type: jsPsychPreload,
@@ -120,6 +137,13 @@ const PRELOAD_VIDEOS = {
 ////////////////////////////////////////////////////////////////////////
 //                              Exp Parts                             //
 ////////////////////////////////////////////////////////////////////////
+
+function canvas_style(ctx) {
+    ctx.fillStyle = CANVAS_COLOUR;
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2); // make center (0, 0) for easier positioning!
+    return ctx;
+}
 
 function draw_fixation(c) {
     "use strict";
@@ -144,57 +168,172 @@ const FIXATION_CROSS = {
     trial_duration: PRMS.fix_duration,
 };
 
-// const TRIAL_FEEDBACK = {
-//     type: jsPsychHtmlKeyboardResponseCanvas,
-//     canvas_colour: CANVAS_COLOUR,
-//     canvas_size: CANVAS_SIZE,
-//     canvas_border: CANVAS_BORDER,
-//     stimulus: "",
-//     response_ends_trial: false,
-//     trial_duration: 0,
-//     on_start: function (trial) {
-//         let dat = jsPsych.data.get().last(1).values()[0];
-//         trial.trial_duration = PRMS.fbDur[dat.error];
-//         trial.stimulus = generate_formatted_html({
-//             text: `${PRMS.fbTxt[dat.error]}`,
-//             align: "center",
-//             fontsize: 30,
-//         });
-//     },
-// };
+// jsPsych video plugin
+const PLAY_VIDEO = {
+    type: jsPsychVideoKeyboardResponse,
+    stimulus: [],
+    data: {
+        stim_type: "gc",
+        video: jsPsych.timelineVariable("video"),
+        resp_modality: jsPsych.timelineVariable("resp_modality"),
+        voice: jsPsych.timelineVariable("voice"),
+        gesture: jsPsych.timelineVariable("gesture"),
+        comp: jsPsych.timelineVariable("comp"),
+        correct_key: jsPsych.timelineVariable("correct_key"),
+    },
+    on_start: function (trial) {
+        "use strict";
+        trial.stimulus = [jsPsych.evaluateTimelineVariable("video")];
+    },
+    choices: PRMS.resp_keys,
+    response_ends_trial: true,
+    trial_ends_after_video: false,
+    on_finish: function () {
+        code_trial();
+        PRMS.ctrl += 1;
+    },
+};
 
-// const BLOCK_FEEDBACK = {
-//     type: jsPsychHtmlKeyboardResponseCanvas,
-//     canvas_colour: CANVAS_COLOUR,
-//     canvas_size: CANVAS_SIZE,
-//     canvas_border: CANVAS_BORDER,
-//     response_ends_trial: true,
-//     stimulus: "",
-//     choices: [" "],
-//     on_start: function (trial) {
-//         let block_dvs = calculateBlockPerformance({
-//             filter_options: { stim: "gc", blockNum: PRMS.cblk },
-//             corrColumn: "error",
-//             corrValue: 0,
-//         });
-//         trial.stimulus = generate_formatted_html({
-//             text: `Ende Block ${PRMS.cblk} von ${PRMS.nblks}<br><br>
-//             Mittlere Reaktionzeit: ${block_dvs.meanRt} ms<br>
-//             Fehlerrate: ${block_dvs.errorRate}%<br><br>
-//             Drücken Sie die Leertaste, um fortzufahren.`,
-//             align: "center",
-//             fontsize: 30,
-//         });
-//     },
-//     on_finish: function () {
-//         PRMS.ctrl = 1;
-//         PRMS.cblk += 1;
-//     },
-// };
+function code_trial() {
+    "use strict";
 
-const TRIAL_TIMELINE = {
-    timeline: [FIXATION_CROSS, ITI],
-    timeline_variables: TRIAL_TABLE,
+    let dat = jsPsych.data.get().last(1).values()[0];
+    dat.rt = dat.rt !== null ? dat.rt : PRMS.too_slow;
+
+    let corr_code = 0;
+    let correct_key = jsPsych.pluginAPI.compareKeys(dat.response, dat.correct_key);
+
+    if (correct_key && dat.rt > PRMS.too_fast && dat.rt < PRMS.too_slow) {
+        corr_code = 1; // correct
+    } else if (!correct_key && dat.rt > PRMS.too_fast && dat.rt < PRMS.too_slow) {
+        corr_code = 2; // choice error
+    } else if (dat.rt >= PRMS.too_slow) {
+        corr_code = 3; // too slow
+    } else if (dat.rt <= PRMS.too_fast) {
+        corr_code = 4; // too fast
+    }
+
+    // console.log(`-------`);
+    // console.log(`video: ${dat.video}`);
+    // console.log(`resp_modality: ${dat.resp_modality}`);
+    // console.log(`voice: ${dat.voice}`);
+    // console.log(`gesture: ${dat.gesture}`);
+    // console.log(`comp: ${dat.comp}`);
+    // console.log(`correct_key: ${dat.correct_key}`);
+    // console.log(`rt: ${dat.rt}`);
+    // console.log(`corr_code: ${corr_code}`);
+
+    jsPsych.data.addDataToLastTrial({
+        date: Date(),
+        block_num: PRMS.cblk,
+        trial_num: PRMS.ctrl,
+        corr_code: corr_code,
+    });
+}
+
+function draw_trial_feedback(c, args) {
+    "use strict";
+    console.log("here");
+    let ctx = c.getContext("2d");
+    ctx = canvas_style(ctx);
+
+    ctx.font = PRMS.feedback_font;
+    ctx.textAlign = "center";
+
+    // draw target
+    ctx.fillStyle = "black";
+    ctx.fillText(`${args.feedback_text}`, 0, 15);
+}
+
+const TRIAL_FEEDBACK = {
+    type: jsPsychCanvasKeyboardResponse,
+    stimulus: draw_iti,
+    canvas_size: CANVAS_SIZE,
+    response_ends_trial: false,
+    trial_duration: 0,
+    on_start: function (trial) {
+        let dat = jsPsych.data.get().last(1).values()[0];
+        trial.trial_duration = PRMS.feedback_duration[dat.corr_code - 1];
+        trial.stimulus = function (c) {
+            draw_trial_feedback(c, { feedback_text: PRMS.feedback_text[dat.corr_code - 1] });
+        };
+    },
+};
+
+const BLOCK_FEEDBACK = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: "",
+    response_ends_trial: true,
+    on_start: function (trial) {
+        let block_dvs = calculate_block_performance({
+            filter_options: { stim_type: "gc", block_num: PRMS.cblk },
+        });
+        let text = block_feedback_text(
+            PRMS.cblk,
+            PRMS.nblks,
+            block_dvs.mean_rt,
+            block_dvs.error_rate,
+            (language = "de"),
+        );
+        trial.stimulus = `<div style="color: black; font-size:30px;">${text}</div>`;
+    },
+    on_finish: function () {
+        PRMS.ctrl = 1;
+        PRMS.cblk += 1;
+    },
+    post_trial_gap: 1000,
+};
+
+function draw_iti(c) {
+    "use strict";
+    let ctx = c.getContext("2d");
+    canvas_style(ctx);
+}
+
+const ITI = {
+    type: jsPsychCanvasKeyboardResponse,
+    canvas_size: CANVAS_SIZE,
+    stimulus: draw_iti,
+    response_ends_trial: false,
+    trial_duration: PRMS.iti,
+};
+
+// prettier-ignore
+const TRIAL_TABLE_VOICE = [
+  { video: VIDEOS[0], resp_modality: "voice", voice: "yes", gesture: "yes", comp: "comp",   correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Ja")]},
+  { video: VIDEOS[1], resp_modality: "voice", voice: "no",  gesture: "yes", comp: "incomp", correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Nein")]},
+  { video: VIDEOS[2], resp_modality: "voice", voice: "yes", gesture: "yes", comp: "comp",   correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Ja")]},
+  { video: VIDEOS[3], resp_modality: "voice", voice: "no",  gesture: "yes", comp: "incomp", correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Nein")]},
+  { video: VIDEOS[4], resp_modality: "voice", voice: "yes", gesture: "no",  comp: "incomp", correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Ja")]},
+  { video: VIDEOS[5], resp_modality: "voice", voice: "no",  gesture: "no",  comp: "comp",   correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Nein")]},
+  { video: VIDEOS[6], resp_modality: "voice", voice: "yes", gesture: "no",  comp: "incomp", correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Ja")]},
+  { video: VIDEOS[7], resp_modality: "voice", voice: "no",  gesture: "no",  comp: "comp",   correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Nein")]},
+];
+
+// prettier-ignore
+const TRIAL_TABLE_GESTURE = [
+  { video: VIDEOS[0], resp_modality: "gesture", voice: "yes", gesture: "yes", comp: "comp",   correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Ja")]},
+  { video: VIDEOS[1], resp_modality: "gesture", voice: "no",  gesture: "yes", comp: "incomp", correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Ja")]},
+  { video: VIDEOS[2], resp_modality: "gesture", voice: "yes", gesture: "yes", comp: "comp",   correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Ja")]},
+  { video: VIDEOS[3], resp_modality: "gesture", voice: "no",  gesture: "yes", comp: "incomp", correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Ja")]},
+  { video: VIDEOS[4], resp_modality: "gesture", voice: "yes", gesture: "no",  comp: "incomp", correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Nein")]},
+  { video: VIDEOS[5], resp_modality: "gesture", voice: "no",  gesture: "no",  comp: "comp",   correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Nein")]},
+  { video: VIDEOS[6], resp_modality: "gesture", voice: "yes", gesture: "no",  comp: "incomp", correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Nein")]},
+  { video: VIDEOS[7], resp_modality: "gesture", voice: "no",  gesture: "no",  comp: "comp",   correct_key: PRMS.resp_keys[PRMS.resp_mapping.indexOf("Nein")]},
+];
+
+// useful debugging commands
+// console.table(TRIAL_TABLE_VOICE);
+// console.table(TRIAL_TABLE_GESTURE);
+
+const TRIAL_TIMELINE_VOICE = {
+    timeline: [FIXATION_CROSS, PLAY_VIDEO, TRIAL_FEEDBACK, ITI],
+    timeline_variables: TRIAL_TABLE_VOICE,
+};
+
+const TRIAL_TIMELINE_GESTURE = {
+    timeline: [FIXATION_CROSS, PLAY_VIDEO, TRIAL_FEEDBACK, ITI],
+    timeline_variables: TRIAL_TABLE_GESTURE,
 };
 
 ////////////////////////////////////////////////////////////////////////
@@ -208,8 +347,8 @@ function save() {
     jsPsych.data.addProperties({ vp_num: VP_NUM });
 
     const data_fn = `${DIR_NAME}data/${EXP_NAME}_${VP_NUM}`;
-    save_data_server("/Common8+/write_data.php", data_fn, { stim_type: "gc" });
-    // save_data_local(data_fn, { stim_type: "gc" });  // saves to download folder
+    // save_data_server("/Common8+/write_data.php", data_fn, { stim_type: "gc" });
+    save_data_local(data_fn, { stim_type: "gc" }); // saves to download folder
 }
 
 const SAVE_DATA = {
@@ -230,7 +369,7 @@ const END_SCREEN = {
 Nun folgen Informationen zur Versuchspersonenstunde auf SONA.
 Drücken Sie nun eine beliebige Taste, um fortzufahren.`,
         fontsize: 28,
-        color: PRMS.colours.mid,
+        color: "black",
         lineheight: 1.0,
         bold: false,
         align: "left",
@@ -248,13 +387,40 @@ function generate_exp() {
     let exp = [];
 
     exp.push(fullscreen(true));
-    exp.push(browser_check(CANVAS_SIZE));
+    exp.push(browser_check([CANVAS_SIZE[1], CANVAS_SIZE[0]]));
     exp.push(resize_browser());
     exp.push(welcome_message());
     // exp.push(vp_info_form("/Common8+/vpInfoForm_de.html"));
     exp.push(mouse_cursor(false));
     exp.push(WELCOME_INSTRUCTIONS);
     exp.push(TASK_INSTRUCTIONS);
+
+    // alternating block types with random assignment of starting order
+    let blk_type;
+    if ([1, 3].includes(VERSION)) {
+        blk_type = repeat_array(["voice", "gesture"], PRMS.nblks / 2);
+    } else if ([2, 4].includes(VERSION)) {
+        blk_type = repeat_array(["gesture", "voice"], PRMS.nblks / 2);
+    }
+
+    for (let blk = 0; blk < PRMS.nblks; blk += 1) {
+        let blk_timeline;
+        if (blk_type[blk] === "voice") {
+            blk_timeline = { ...TRIAL_TIMELINE_VOICE };
+            blk_timeline.sample = {
+                type: "fixed-repetitions",
+                size: PRMS.ntrls / TRIAL_TABLE_VOICE.length,
+            };
+        } else if (blk_type[blk] === "gesture") {
+            blk_timeline = { ...TRIAL_TIMELINE_GESTURE };
+            blk_timeline.sample = {
+                type: "fixed-repetitions",
+                size: PRMS.ntrls / TRIAL_TABLE_GESTURE.length,
+            };
+        }
+        exp.push(blk_timeline); // trials within a block
+        exp.push(BLOCK_FEEDBACK); // show previous block performance
+    }
 
     // save data
     exp.push(SAVE_DATA);
@@ -269,6 +435,6 @@ function generate_exp() {
 }
 const EXP = generate_exp();
 
-// jsPsych.simulate(EXP, "data-only");
+// jsPsych.simulate(EXP, "data-only"); // generatees datafile after first click
 // jsPsych.simulate(EXP, "visual");
 jsPsych.run(EXP);
