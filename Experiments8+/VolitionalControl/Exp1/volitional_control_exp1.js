56c048201ba4d2effb8e8a2a0d93051e054bd9fe
igmmgi <ian.mackenzie@uni-tuebingen.de>
2024-11-28 10:48:29 +0100

    updated instructions + sizes

--- Experiments8+/VolitionalControl/Exp1/volitional_control_exp1.js
+++ Experiments8+/VolitionalControl/Exp1/volitional_control_exp1.js
@@ -14,36 +14,36 @@ const CANVAS_SIZE = [720, 1280]; // height,width
 ////////////////////////////////////////////////////////////////////////
 const PRMS = {
   ntrlsp: 8, // number of trials in first block (practice)
-  ntrlse: 40, // number of trials in subsequent blocks
-  nblks: 8,
+  ntrlse: 40, // number of trials in subsequent blocks (40)
+  nblks: 8, // number of blocks (8)
   fix_size: 20, // size of the fixation cross
   fix_width: 5, // width of fixation cross
-  fix_duration: [800, 500], // duration of the fixation cross
+  fix_duration: [1000, 650], // duration of the fixation cross
   fix_colour: "Black", // colour of the fixation cross
   cue_duration: 1000, // duration of the cue
   cue_size: 40, // size of the cue
   cue_colours: shuffle(["Cyan", "Yellow"]),
   choice_feedback_duration: 1000,
-  image_eccentricity: 185,
+  image_eccentricity: 165, // 185
   image_size: 275, // image size in pixels (original image = 200)
-  feedback_duration: 1000,
+  selected_picture_duration: 1000,
   frame_size: [300, 300],
-  frame_width: 10,
-  frame_colour: "White",
+  frame_width: 20,
+  frame_colour: ["Gray", "White"], // ["LightGray", "White"], // 1st is only for initial auto selection in forced
   wait_duration: 1000,
   iti: 1000,
   choice_selection_keys: ["E", "P"],
   task_keys: ["E", "P"],
   task_shapes: shuffle(["circle", "diamond"]),
-  task_colour: "White",
+  task_colour: "Black", // White
   task_size: 80,
-  task_eccentricity: 400,
+  task_eccentricity: 250, // 400
   task_duration: 300,
   too_fast: 0,
-  too_slow: 1000,
+  too_slow: 1500, // actually 1500 from onset on stimulus (selected_picture_duration)!
   feedback_font: "50px Arial",
-  feedback_text: ["Richtig!", "Falsch!", "Zu langsam!", "Zu schnell!"],
-  feedback_duration: [1000, 1000, 1000, 1000], // feedback duration for response type (correct, incorrect, too slow, too fast)
+  feedback_text: ["", "Falsch!", "Zu langsam!", "Zu schnell!"],
+  feedback_duration: [0, 1000, 1000, 1000], // feedback duration for response type (correct, incorrect, too slow, too fast)
   ctrl: 1, // count trials
   cblk: 1, // count blocks
   t_interval: 100,
@@ -51,8 +51,10 @@ const PRMS = {
 };
 
 // 2 counter balanced versions
-const VERSION = 1; //Number(jsPsych.data.urlVariables().version);
+const VERSION = Number(jsPsych.data.urlVariables().version);
 jsPsych.data.addProperties({ version: VERSION });
+
+const EN_DE = {"Cyan": "Blau", "Yellow": "Gelb", "circle": "Kreis", "diamond": "Raute"};
 
 
 ////////////////////////////////////////////////////////////////////////
@@ -65,10 +67,8 @@ const WELCOME_INSTRUCTIONS = {
     text: `Willkommen zu unserem Experiment:<br><br>
 Die Teilnahme ist freiwillig und Du darfst das Experiment jederzeit abbrechen.
 Bitte stelle sicher, dass Du Dich in einer ruhigen Umgebung befindest und genügend Zeit hast,
-um das Experiment durchzuführen. Wir bitten Dich, die nächsten ca. 30-35 Minuten konzentriert zu arbeiten.<br><br>
-Informationen zur Versuchspersonenstunde erhälst Du nach dem Experiment.
-Bei Fragen oder Problemen wende Dich bitte an:<br><br>
-XXX.XXX<br><br>
+um das Experiment durchzuführen. Wir bitten Dich, die nächsten ca. 40 Minuten konzentriert zu arbeiten.<br><br>
+Du wirst nach dem Experiment zu SONA zurückgeleitet und solltest dann deine VP-Stunde erhalten.<br><br>
 Drücke eine beliebige Taste, um fortzufahren`,
     align: "left",
     color: "Black",
@@ -79,14 +79,21 @@ Drücke eine beliebige Taste, um fortzufahren`,
 };
 
 
-const TASK_INSTRUCTIONS = {
+const TASK_INSTRUCTIONS_VISIBLE = {
   type: jsPsychHtmlKeyboardResponse,
   canvas_size: CANVAS_SIZE,
   stimulus: "",
   post_trial_gap: 1000,
   on_start: function (trial) {
     trial.stimulus = generate_formatted_html({
-      text: `Instructions ... <br>
+      text: `In jedem Durchgang siehst du zunächst einen gelben oder blauen Kreis und dann zwei Bilder. <br><br>
+Wenn der Kreis <span style="color: ${PRMS.cue_colours[0]}; font-weight: bold" >${EN_DE[PRMS.cue_colours[0]]} </span>ist, darfst du frei eines der zwei Bilder wählen.<br><br>
+Wenn der Kreis <span style="color: ${PRMS.cue_colours[1]}; font-weight: bold" >${EN_DE[PRMS.cue_colours[1]]} </span>ist, dann musst du das Bild wählen welches einen grauen Rahmen hat.<br><br>
+Es gilt:<br><br>
+Kreis <span style="color: ${PRMS.cue_colours[0]}; font-weight: bold" >${EN_DE[PRMS.cue_colours[0]]}</span>: Freie Wahl<br>
+Kreis <span style="color: ${PRMS.cue_colours[1]}; font-weight: bold" >${EN_DE[PRMS.cue_colours[1]]}: </span>vorgebene Wahl<br><br>
+Wahl linkes Bild: <span style="font-weight: bold">E-Taste</span> (linker Zeigefinger)<br>
+Wahl rechtes Bild: <span style="font-weight: bold">P-Taste</span> (rechter Zeigefinger)<br><br>
 Drücke eine beliebige Taste, um fortzufahren`,
       align: "left",
       color: "Black",
@@ -96,6 +103,51 @@ Drücke eine beliebige Taste, um fortzufahren`,
   },
 };
 
+
+const TASK_INSTRUCTIONS_BLANK = {
+  type: jsPsychHtmlKeyboardResponse,
+  canvas_size: CANVAS_SIZE,
+  stimulus: "",
+  post_trial_gap: 1000,
+  on_start: function (trial) {
+    trial.stimulus = generate_formatted_html({
+      text: `In jedem Durchgang siehst du zunächst einen gelben oder blauen Kreis und dann zwei <span style="font-weight:bold">SCHWARZE</span> Flächen hinter denen sich zwei Bilder befinden. <br><br>
+Wenn der Kreis <span style="color: ${PRMS.cue_colours[0]}; font-weight: bold" >${EN_DE[PRMS.cue_colours[0]]}</span> ist, darfst du frei eines der zwei Bilder wählen.<br><br>
+Wenn der Kreis <span style="color: ${PRMS.cue_colours[1]}; font-weight: bold" >${EN_DE[PRMS.cue_colours[1]]}</span> ist, dann musst du das Bild wählen welches einen grauen Rahmen hat.<br><br>
+Es gilt:<br><br>
+Kreis <span style="color: ${PRMS.cue_colours[0]}; font-weight: bold" >${EN_DE[PRMS.cue_colours[0]]}</span>: Freie Wahl<br>
+Kreis <span style="color: ${PRMS.cue_colours[1]}; font-weight: bold" >${EN_DE[PRMS.cue_colours[1]]}: </span>vorgebene Wahl<br><br>
+Wahl linkes Bild: <span style="font-weight: bold">E-Taste</span> (linker Zeigefinger)<br>
+Wahl rechtes Bild: <span style="font-weight: bold">P-Taste</span> (rechter Zeigefinger)<br><br>
+Drücke eine beliebige Taste, um fortzufahren`,
+      align: "left",
+      color: "Black",
+      fontsize: 30,
+      bold: false,
+    });
+  },
+};
+
+
+const TASK_INSTRUCTIONS = {
+  type: jsPsychHtmlKeyboardResponse,
+  canvas_size: CANVAS_SIZE,
+  stimulus: "",
+  post_trial_gap: 1000,
+  on_start: function (trial) {
+    trial.stimulus = generate_formatted_html({
+      text: `Nach dem ein Bild als Hintergrund gewählt wurde musst du auf eine weiß Form reagieren, welche rechts oder links auf dem Bildschirm präsentiert wird.<br><br>
+Reagiere so schnell und genau wie möglich wie folgt:<br><br>
+${EN_DE[PRMS.task_shapes[0]]}: E-Taste (linker Zeigefinger)<br>
+${EN_DE[PRMS.task_shapes[1]]}: P-Taste (rechter Zeigefinger)<br><br>
+Drücke eine beliebige Taste, um fortzufahren`,
+      align: "left",
+      color: "Black",
+      fontsize: 30,
+      bold: false,
+    });
+  },
+};
 
 const BLOCK_START = {
   type: jsPsychHtmlKeyboardResponse,
@@ -104,7 +156,15 @@ const BLOCK_START = {
   post_trial_gap: 1000,
   on_start: function (trial) {
     trial.stimulus = generate_formatted_html({
-      text: `Block Start: ${PRMS.cblk} von ${PRMS.nblks}<br><br>
+      text: `Block ${PRMS.cblk} von ${PRMS.nblks}<br><br>
+Wähle entweder ein Bild frei aus oder das vorgegebene von zwei Bildern:<br><br>
+Kreis <span style="color: ${PRMS.cue_colours[0]}; font-weight: bold" >${EN_DE[PRMS.cue_colours[0]]}</span>: Freie Wahl<br>
+Kreis <span style="color: ${PRMS.cue_colours[1]}; font-weight: bold" >${EN_DE[PRMS.cue_colours[1]]}: </span>vorgebene Wahl<br><br>
+Wahl rechtes Bild: E-Taste (linker Zeigefinger)<br>
+Wahl linkes Bild: P-Taste (rechter Zeigefinger)<br><br>
+Bearbeite anschließend die Form-Aufgabe:<br><br>
+${EN_DE[PRMS.task_shapes[0]]}: E-Taste (linker Zeigefinger)<br>
+${EN_DE[PRMS.task_shapes[1]]}: P-Taste (rechter Zeigefinger)<br><br>
 Drücke eine beliebige Taste, um fortzufahren`,
       align: "left",
       color: "Black",
@@ -113,6 +173,25 @@ Drücke eine beliebige Taste, um fortzufahren`,
     });
   },
 };
+
+
+const NEW_INSTRUCTIONS = {
+  type: jsPsychHtmlKeyboardResponse,
+  canvas_size: CANVAS_SIZE,
+  stimulus: "",
+  post_trial_gap: 1000,
+  on_start: function (trial) {
+    trial.stimulus = generate_formatted_html({
+      text: `<span style="font-weight:bold">*** ACHTUNG: ES FOLGEN NEUE INSTRUKTIONEN ***</span><br><br>
+Drücke eine beliebige Taste, um fortzufahren`,
+      align: "left",
+      color: "Black",
+      fontsize: 30,
+      bold: false,
+    });
+  },
+};
+
 
 
 function canvas_style(ctx) {
@@ -226,32 +305,31 @@ function draw_images(c, args) {
   let ctx = c.getContext("2d");
   ctx = canvas_style(ctx);
 
-  // image
-  const img_left = new Image();
-  img_left.src = args.choice_type === "free" ? IMAGES_FREE[0] : IMAGES_FORCED[0];
-  const img_right = new Image();
-  img_right.src =  args.choice_type === "free" ? IMAGES_FREE[1] : IMAGES_FORCED[1];
-
-  //ctx.drawImage(img_left, -PRMS.image_eccentricity - img_left.width / 2, -img_left.height / 2);
-  //ctx.drawImage(img_right, PRMS.image_eccentricity - img_left.width / 2, -img_left.height / 2);
-
-  ctx.drawImage(img_left, -PRMS.image_eccentricity - PRMS.image_size/ 2, -PRMS.image_size/2, PRMS.image_size, PRMS.image_size);
-  ctx.drawImage(img_right, PRMS.image_eccentricity - PRMS.image_size/ 2, -PRMS.image_size/2, PRMS.image_size, PRMS.image_size);
-
   // hack to hide images
-  if (args.hide_image) {
+  if (args.hide_image) { // draw rect same size as images
     ctx.beginPath();
     ctx.fillStyle = "Black";
-    ctx.rect(-PRMS.image_eccentricity - img_left.width / 2, -img_left.height / 2, img_left.width, img_left.height); // Add a rectangle to the current path
-    ctx.rect( PRMS.image_eccentricity - img_left.width / 2, -img_left.height / 2, img_left.width, img_left.height); // Add a rectangle to the current path
+    // ctx.rect(-PRMS.image_eccentricity - img_left.width / 2, -img_left.height / 2, img_left.width, img_left.height); // Add a rectangle to the current path
+    // ctx.rect( PRMS.image_eccentricity - img_left.width / 2, -img_left.height / 2, img_left.width, img_left.height); // Add a rectangle to the current path
+    ctx.rect(-PRMS.image_eccentricity - PRMS.image_size/2, -PRMS.image_size/2, PRMS.image_size, PRMS.image_size); // Add a rectangle to the current path
+    ctx.rect( PRMS.image_eccentricity - PRMS.image_size/2, -PRMS.image_size/2, PRMS.image_size, PRMS.image_size); // Add a rectangle to the current path
     ctx.fill(); // Render the path
+  } else {
+
+    // image
+    const img_left = new Image();
+    img_left.src = args.choice_type === "free" ? IMAGES_FREE[0] : IMAGES_FORCED[0];
+    const img_right = new Image();
+    img_right.src =  args.choice_type === "free" ? IMAGES_FREE[1] : IMAGES_FORCED[1];
+    ctx.drawImage(img_left, -PRMS.image_eccentricity - PRMS.image_size/ 2, -PRMS.image_size/2, PRMS.image_size, PRMS.image_size);
+    ctx.drawImage(img_right, PRMS.image_eccentricity - PRMS.image_size/ 2, -PRMS.image_size/2, PRMS.image_size, PRMS.image_size);
   }
 
   // draw selection feedback rectangle
   if (args.selection === "left") {
     ctx.beginPath();
-    ctx.strokeStyle = PRMS.frame_colour;
-    ctx.lineWidth = PRMS.frame_width * args.frame_size;
+    ctx.lineWidth = PRMS.frame_width;
+    ctx.strokeStyle = args.frame_colour;
     ctx.rect(
       -(PRMS.frame_size[0] / 2) - PRMS.image_eccentricity,
       -PRMS.frame_size[1] / 2,
@@ -261,8 +339,8 @@ function draw_images(c, args) {
     ctx.stroke();
   } else if (args.selection === "right") {
     ctx.beginPath();
-    ctx.strokeStyle = PRMS.frame_colour;
-    ctx.lineWidth = PRMS.frame_width * args.frame_size;
+    ctx.lineWidth = PRMS.frame_width;
+    ctx.strokeStyle = args.frame_colour;
     ctx.rect(
       -(PRMS.frame_size[0] / 2) + PRMS.image_eccentricity,
       -PRMS.frame_size[1] / 2,
@@ -354,10 +432,11 @@ const CHOICE_IMAGE_FEEDBACK_SCREEN1 = {
       trial.stimulus = function (c) {
         draw_images(c, { 
           choice_type: jsPsych.evaluateTimelineVariable("choice_type"), 
-          hide_image: false, 
+          hide_image: jsPsych.evaluateTimelineVariable("block") === "blank", 
+          // hide_image: false, 
           selection: 
           dat.selection_side, 
-          frame_size: 1
+          frame_colour: PRMS.frame_colour[1]
         });
       };
     } else if (jsPsych.evaluateTimelineVariable("choice_type") === "forced") {
@@ -366,9 +445,10 @@ const CHOICE_IMAGE_FEEDBACK_SCREEN1 = {
       trial.stimulus = function (c) {
         draw_images(c, {
           choice_type: jsPsych.evaluateTimelineVariable("choice_type"), 
-          hide_image: false, 
+          hide_image: jsPsych.evaluateTimelineVariable("block") === "blank", 
+          //hide_image: false, 
           selection: dat.selection_side, 
-          frame_size: 0.5});
+          frame_colour: PRMS.frame_colour[0]});
       };
     }
   },
@@ -395,9 +475,10 @@ const CHOICE_IMAGE_FEEDBACK_SCREEN2 = {
     trial.stimulus = function (c) {
       draw_images(c, {
         choice_type: jsPsych.evaluateTimelineVariable("choice_type"), 
-        hide_image: false, 
+        hide_image: jsPsych.evaluateTimelineVariable("block") === "blank", 
+        // hide_image: false, 
         selection: dat.selection_side, 
-        frame_size: 1
+        frame_colour: PRMS.frame_colour[1]
       });
     };
   },
@@ -458,7 +539,7 @@ const TASK_SCREEN = {
   stimulus: null,
   canvas_size: CANVAS_SIZE,
   response_ends_trial: true,
-  trial_duration: PRMS.too_slow,
+  trial_duration: PRMS.selected_picture_duration + PRMS.too_slow,
   choices: PRMS.task_keys,
   data: {
     stim_type: "vct",
@@ -473,16 +554,26 @@ const TASK_SCREEN = {
     "use strict";
     trial.stimulus = function (c) {
       let dat = jsPsych.data.get().last(4).values()[0];
-      console.log(dat.selection_side);
       draw_images_and_task(c, 
         { 
           choice_type: jsPsych.evaluateTimelineVariable("choice_type"), 
           selection: dat.selection_side, 
           task_shape: jsPsych.evaluateTimelineVariable("task_shape"),
           task_position: jsPsych.evaluateTimelineVariable("task_position"),
-          draw_stimulus: true,
+          draw_stimulus: false,
           set_canvas: true
         });
+      jsPsych.pluginAPI.setTimeout(() => {
+        draw_images_and_task(c, 
+          { 
+            choice_type: jsPsych.evaluateTimelineVariable("choice_type"), 
+            selection: dat.selection_side, 
+            task_shape: jsPsych.evaluateTimelineVariable("task_shape"),
+            task_position: jsPsych.evaluateTimelineVariable("task_position"),
+            draw_stimulus: true,
+            set_canvas: false
+          });
+      }, PRMS.selected_picture_duration);
       jsPsych.pluginAPI.setTimeout(() => {
         draw_images_and_task(c, 
           { 
@@ -493,7 +584,7 @@ const TASK_SCREEN = {
             draw_stimulus: false,
             set_canvas: false
           });
-      }, PRMS.task_duration);
+      }, PRMS.selected_picture_duration + PRMS.task_duration);
     };
   },
   on_finish: function () {
@@ -509,7 +600,7 @@ function code_task_response() {
   // get some variables from previous trial
   let datp = jsPsych.data.get().last(5).values()[0];
   let dat = jsPsych.data.get().last(1).values()[0];
-  dat.rt = dat.rt !== null ? dat.rt : PRMS.too_slow;
+  dat.rt = dat.rt !== null ? dat.rt - PRMS.selected_picture_duration: PRMS.too_slow;
 
   let corr_code = 0;
   let correct_key = jsPsych.pluginAPI.compareKeys(dat.response, dat.correct_key);
@@ -631,10 +722,10 @@ const TRIAL_TABLE_BLANK = [
   { block: "blank", cue: PRMS.cue_colours[0], choice_type: "free",   task_shape: PRMS.task_shapes[1], task_position: "left",  correct_key: PRMS.task_keys[PRMS.task_shapes.indexOf(PRMS.task_shapes[1])]},
   { block: "blank", cue: PRMS.cue_colours[0], choice_type: "free",   task_shape: PRMS.task_shapes[0], task_position: "right", correct_key: PRMS.task_keys[PRMS.task_shapes.indexOf(PRMS.task_shapes[0])]},
   { block: "blank", cue: PRMS.cue_colours[0], choice_type: "free",   task_shape: PRMS.task_shapes[1], task_position: "right", correct_key: PRMS.task_keys[PRMS.task_shapes.indexOf(PRMS.task_shapes[1])]},
-  { block: "blank", cue: PRMS.cue_colours[1], choice_type: "forced", task_shape: PRMS.task_shapes[0], task_position: "left",  correct_key: PRMS.task_keys[PRMS.task_shapes.indexOf(PRMS.task_shapes[0])]},
-  { block: "blank", cue: PRMS.cue_colours[1], choice_type: "forced", task_shape: PRMS.task_shapes[1], task_position: "left",  correct_key: PRMS.task_keys[PRMS.task_shapes.indexOf(PRMS.task_shapes[1])]},
-  { block: "blank", cue: PRMS.cue_colours[1], choice_type: "forced", task_shape: PRMS.task_shapes[0], task_position: "right", correct_key: PRMS.task_keys[PRMS.task_shapes.indexOf(PRMS.task_shapes[0])]},
-  { block: "blank", cue: PRMS.cue_colours[1], choice_type: "forced", task_shape: PRMS.task_shapes[1], task_position: "right", correct_key: PRMS.task_keys[PRMS.task_shapes.indexOf(PRMS.task_shapes[1])]},
+  // { block: "blank", cue: PRMS.cue_colours[1], choice_type: "forced", task_shape: PRMS.task_shapes[0], task_position: "left",  correct_key: PRMS.task_keys[PRMS.task_shapes.indexOf(PRMS.task_shapes[0])]},
+  // { block: "blank", cue: PRMS.cue_colours[1], choice_type: "forced", task_shape: PRMS.task_shapes[1], task_position: "left",  correct_key: PRMS.task_keys[PRMS.task_shapes.indexOf(PRMS.task_shapes[1])]},
+  // { block: "blank", cue: PRMS.cue_colours[1], choice_type: "forced", task_shape: PRMS.task_shapes[0], task_position: "right", correct_key: PRMS.task_keys[PRMS.task_shapes.indexOf(PRMS.task_shapes[0])]},
+  // { block: "blank", cue: PRMS.cue_colours[1], choice_type: "forced", task_shape: PRMS.task_shapes[1], task_position: "right", correct_key: PRMS.task_keys[PRMS.task_shapes.indexOf(PRMS.task_shapes[1])]},
 ];
 
 
@@ -685,8 +776,6 @@ function generate_exp() {
   exp.push(mouse_cursor(false));
   exp.push(PRELOAD);
   exp.push(WELCOME_INSTRUCTIONS);
-  exp.push(TASK_INSTRUCTIONS);
-
 
   // // block by block
   // let blk_type;
@@ -707,6 +796,16 @@ function generate_exp() {
 
   let practice_blocks = [0, PRMS.nblks/2];
   for (let blk = 0; blk < PRMS.nblks; blk++) {
+    if (blk === PRMS.nblks/2) {
+      exp.push(NEW_INSTRUCTIONS);
+    }
+    if ((blk === 0 && VERSION === 1) || (blk === PRMS.nblks/2 && VERSION == 2)) {
+      exp.push(TASK_INSTRUCTIONS_VISIBLE);
+      exp.push(TASK_INSTRUCTIONS);
+    } else if ((blk === 0 && VERSION === 2) || (blk === PRMS.nblks/2 && VERSION == 1)) {
+      exp.push(TASK_INSTRUCTIONS_BLANK);
+      exp.push(TASK_INSTRUCTIONS);
+    } 
     exp.push(BLOCK_START);
     let blk_timeline;
     if (blk_type[blk] === "image") {
