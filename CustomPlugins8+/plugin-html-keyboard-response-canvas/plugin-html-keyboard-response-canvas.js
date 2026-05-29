7cfcc5d4306bf1bc33d47f3bf816f9d0e12ab5a9
igmmgi <ian.mackenzie@uni-tuebingen.de>
2026-05-29 19:02:20 +0200

    new pilots experiments added

---
+++ CustomPlugins8+/plugin-html-keyboard-response-canvas/plugin-html-keyboard-response-canvas.js
@@ -0,0 +1,177 @@
+var jsPsychHtmlKeyboardResponseCanvas = (function (jspsych) {
+  'use strict';
+
+  const info = {
+    name: 'html-keyboard-response-canvas',
+    version: '1.0.0',
+    parameters: {
+      stimulus: {
+        type: jspsych.ParameterType.HTML_STRING,
+        pretty_name: 'Stimulus',
+        default: undefined,
+        description: 'The HTML string to be displayed',
+      },
+      choices: {
+        type: jspsych.ParameterType.KEYS,
+        pretty_name: 'Choices',
+        default: 'ALL_KEYS',
+        description: 'The keys the subject is allowed to press to respond to the stimulus.',
+      },
+      canvas_colour: {
+        type: jspsych.ParameterType.STRING,
+        array: false,
+        pretty_name: 'Colour',
+        default: 'rgba(100, 100, 100, 0.5)',
+        description: 'Canvas colour.',
+      },
+      canvas_size: {
+        type: jspsych.ParameterType.Array,
+        pretty_name: 'Size',
+        default: [1280, 960],
+        description: 'Canvas size.',
+      },
+      canvas_border: {
+        type: jspsych.ParameterType.STRING,
+        pretty_name: 'Border',
+        default: '10px solid black',
+        description: 'Border style',
+      },
+      translate_origin: {
+        type: jspsych.ParameterType.BOOL,
+        pretty_name: 'Translate',
+        default: false,
+        description: 'Translate origin to center',
+      },
+      stimulus_duration: {
+        type: jspsych.ParameterType.INT,
+        pretty_name: 'Stimulus duration',
+        default: null,
+        description: 'How long to hide the stimulus.',
+      },
+      trial_duration: {
+        type: jspsych.ParameterType.INT,
+        pretty_name: 'Trial duration',
+        default: null,
+        description: 'How long to show trial before it ends.',
+      },
+      response_ends_trial: {
+        type: jspsych.ParameterType.BOOL,
+        pretty_name: 'Response ends trial',
+        default: true,
+        description: 'If true, trial will end when subject makes a response.',
+      },
+    },
+    data: {
+      rt: {
+        type: jspsych.ParameterType.INT
+      },
+      stimulus: {
+        type: jspsych.ParameterType.STRING
+      },
+      key_press: {
+        type: jspsych.ParameterType.STRING
+      }
+    }
+  };
+
+  class HtmlKeyboardResponseCanvasPlugin {
+    constructor(jsPsych) {
+      this.jsPsych = jsPsych;
+    }
+
+    trial(display_element, trial) {
+      // setup canvas
+      let new_html =
+        '<div style="position:relative; width:' + trial.canvas_size[0] + 'px; height:' + trial.canvas_size[1] + 'px; display: flex; justify-content: center; align-items: center;">' +
+        '<canvas id="canvas" width="' +
+        trial.canvas_size[0] +
+        '" height="' +
+        trial.canvas_size[1] +
+        '" style="border: ' +
+        trial.canvas_border +
+        '; position: absolute; z-index: -1; top: 0; left: 0;"></canvas>' +
+        '<div id="stimulus">' + trial.stimulus + '</div>' +
+        '</div>';
+
+      display_element.innerHTML = new_html;
+
+      let canvas = display_element.querySelector('canvas');
+      let ctx = canvas.getContext('2d');
+
+      ctx.fillStyle = trial.canvas_colour;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      if (trial.translate_origin) {
+        ctx.translate(canvas.width / 2, canvas.height / 2); // make center (0, 0)
+      }
+
+      // store response
+      let response = {
+        rt: null,
+        key: null,
+      };
+
+      // function to end trial when it is time
+      const end_trial = () => {
+        // kill any remaining setTimeout handlers
+        this.jsPsych.pluginAPI.clearAllTimeouts();
+
+        // kill keyboard listeners
+        if (typeof keyboardListener !== 'undefined') {
+          this.jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
+        }
+
+        // gather the data to store for the trial
+        let trial_data = {
+          rt: response.rt,
+          stimulus: trial.stimulus,
+          key_press: response.key,
+        };
+
+        // clear the display
+        display_element.innerHTML = '';
+
+        // move on to the next trial
+        this.jsPsych.finishTrial(trial_data);
+      };
+
+      // function to handle responses by the subject
+      let after_response = (info) => {
+        // only record the first response
+        if (response.key == null) {
+          response = info;
+        }
+
+        if (trial.response_ends_trial) {
+          end_trial();
+        }
+      };
+
+      // start the response listener
+      if (trial.choices !== 'NO_KEYS') {
+        var keyboardListener = this.jsPsych.pluginAPI.getKeyboardResponse({
+          callback_function: after_response,
+          valid_responses: trial.choices,
+          rt_method: 'performance',
+          persist: false,
+          allow_held_key: false,
+        });
+      }
+
+      // hide stimulus if stimulus_duration is set
+      if (trial.stimulus_duration !== null) {
+        this.jsPsych.pluginAPI.setTimeout(function () {
+          display_element.querySelector('#stimulus').style.visibility = 'hidden';
+        }, trial.stimulus_duration);
+      }
+
+      // end trial if trial_duration is set
+      if (trial.trial_duration !== null) {
+        this.jsPsych.pluginAPI.setTimeout(end_trial, trial.trial_duration);
+      }
+    }
+  }
+
+  HtmlKeyboardResponseCanvasPlugin.info = info;
+  return HtmlKeyboardResponseCanvasPlugin;
+})(jsPsychModule);
---
+++ Experiments8+/FollowThePath/Version2/Version2.2/FollowThePath.js
@@ -0,0 +1,550 @@
+// Version 2.2 for pilot SFB ironic errors series of experiments
+// Participants are required to control a ball (small circle) that moves from the bottom
+// to the top of the screen. The x-position is controlled  by moving the mouse/trackpad
+// left/right so that the ball follows a path.
+//
+// 3 types of trials:
+// 1) Low noise
+// 2) Medium noise
+// 3) High noise
+// (Global noise type is set in PRMS)
+//
+// Trials are presented in mixed blocks containing all trial types.
+
+const jsPsych = initJsPsych({});
+
+////////////////////////////////////////////////////////////////////////
+//                         Canvas Properties                          //
+////////////////////////////////////////////////////////////////////////
+const CANVAS_COLOUR = "rgba(200, 200, 200, 1)";
+const CANVAS_SIZE = [1280, 720];
+const CANVAS_BORDER = "5px solid black";
+
+const p5js = new p5((sketch) => {
+    sketch.setup = () => {
+    };
+});
+
+const PRMS = {
+    n_trials: 3, // number of trials per block (must be multiple of 3)
+    n_blocks: 4, // total number of mixed blocks
+    iti: 500, // duration of the inter-trial-interval
+    survey_type: "likert", // options: "likert" or "slider"
+    survey_scale: [1, 9],
+    survey_question_1: "Question 1",
+    survey_question_2: "Question 2",
+    survey_anchors: ["Extremely Low", "Extremely High"],
+    font: "50px Arial",
+    colours: { correct: [0, 255, 0], incorrect: [255, 0, 0], path: [150, 150, 150], background: [200, 200, 200] },
+    count_block: 1,
+    count_trial: 1,
+    path_start: 100, // path starts X up from bottom of screen
+    path_width: 10, // width of the path in pixels
+    path_difficulty: { standard: 500 },
+    speed_difficulty: { standard: 1 }, // controlled by frame rate (1px vs 2px per frame)
+    noise_type: "reversal", // option: "reversal" or "gaussian"
+    noise_reversal_prob: { low: 0.10, med: 0.20, high: 0.30 }, // probability of reversing mouse movement
+    noise_gaussian_sd: { low: 2, med: 4, high: 6 }, // SD of gaussian noise added to movement
+    ball_diameter: 20,
+    show_ball_path: true, // the black line the ball travelled
+    show_error_path: true, // the red/green path feedback
+    show_percentage_path: true, // display percentage time on path after trial
+    show_condition_info: true, // display condition info for piloting
+    hide_cursor: true, // hide the real mouse cursor when moving the ball
+    distance_criterion: 15, // error criterion if shown
+    scale_factor: 1, // default scale factor if SCALE_FACTOR trial is skipped
+};
+
+////////////////////////////////////////////////////////////////////////
+//                      Experiment Instructions                       //
+////////////////////////////////////////////////////////////////////////
+const WELCOME_INSTRUCTIONS = {
+    type: jsPsychHtmlKeyboardResponseCanvas,
+    canvas_colour: CANVAS_COLOUR,
+    canvas_size: CANVAS_SIZE,
+    canvas_border: CANVAS_BORDER,
+    stimulus: generate_formatted_html({
+        text: `Willkommen zu unserem Experiment:<br><br>
+               Die Teilnahme ist freiwillig und du darfst das Experiment jederzeit abbrechen.
+               Bitte stelle sicher, dass du dich in einer ruhigen Umgebung befindest und genügend Zeit hast,
+               um das Experiment durchzuführen. Wir bitten dich die nächsten ca. 30-35 Minuten konzentriert zu arbeiten.<br><br>
+               Du erhältst Informationen zur Versuchspersonenstunde nach dem Experiment.
+               Bei Fragen oder Problemen wende dich bitte an:<br><br>
+               xxx.xxx<br><br>
+               Drücke eine beliebige Taste, um fortzufahren`,
+        align: "left",
+        colour: "black",
+        fontsize: 30,
+    }),
+};
+
+////////////////////////////////////////////////////////////////////////
+//                      Experiment Parts                              //
+////////////////////////////////////////////////////////////////////////
+function get_scale_factor() {
+    let dat = jsPsych.data.get().last(1).values()[0];
+    PRMS.scale_factor = 1 / dat.scale_factor;
+    jsPsych.data.addProperties({ scale_factor: PRMS.scale_factor });
+}
+
+const SCALE_FACTOR = {
+    type: jsPsychCallFunction,
+    func: get_scale_factor,
+};
+
+class Path {
+    constructor(step) {
+        this.reset(step);
+    }
+
+    reset(step) {
+        this.x = new Array(Math.round((CANVAS_SIZE[1] - PRMS.path_start) / step)).fill(0);
+        this.y = range(CANVAS_SIZE[1] - PRMS.path_start, 0, -step);
+        this.on_path = new Array(Math.round((CANVAS_SIZE[1] - PRMS.path_start) / step)).fill(0);
+    }
+
+    // position within canvas and scale
+    perlin_noise_coordinates(noise_value, speed_value) {
+        let xpos = CANVAS_SIZE[0] * 0.75;
+        let start = Math.round(Math.random() * xpos);
+        for (let i = start; i < start + this.x.length; i++) {
+            this.x[i - start] = p5js.noise((i / noise_value) * speed_value) * xpos + (CANVAS_SIZE[0] / 2) - (xpos / 2);
+        }
+    }
+
+    calculate_distance(x, y, criterion) {
+        if (y > CANVAS_SIZE[1] - PRMS.path_start) return;
+        let distance = Math.abs(Math.round(x) - PATH.x[y]);
+        this.on_path[y - 1] = distance < criterion ? 1 : -1;
+    }
+
+    draw_target_path() {
+        p5js.strokeWeight(PRMS.path_width);
+        for (let x = this.x.length - 1; x > 0; x--) {
+            if (this.on_path[x] === 0 || !PRMS.show_error_path) {
+                p5js.stroke(...PRMS.colours.path);
+            } else if (this.on_path[x] === 1) {
+                p5js.stroke(...PRMS.colours.correct);
+            } else if (this.on_path[x] === -1) {
+                p5js.stroke(...PRMS.colours.incorrect);
+            }
+            p5js.line(this.x[x - 1], this.y[x - 1], this.x[x], this.y[x]);
+        }
+    }
+}
+
+class Ball {
+    constructor(length) {
+        this.reset(length);
+    }
+
+    reset(length) {
+        this.length = length;
+        this.is_moving = false;
+        this.is_complete = false;
+        this.diameter = PRMS.ball_diameter;
+        this.speed = null;
+        this.noise_type = null;
+        this.noise_level = null;
+        this.step = 0;
+        this.x = null;
+        this.y = CANVAS_SIZE[1] - PRMS.ball_diameter;
+        this.x_path = [];
+        this.y_path = [];
+    }
+
+    set_x_position(x_pos) {
+        this.x = x_pos;
+    }
+    set_speed(speed) {
+        this.speed = speed;
+    }
+
+    move() {
+        // trial initiation
+        if (!this.is_moving && p5js.mouseIsPressed) {
+            let dx = Math.abs(p5js.mouseX * PRMS.scale_factor) - this.x - 5;
+            let dy = Math.abs(p5js.mouseY * PRMS.scale_factor) - this.y - 5;
+            let distance = Math.sqrt(dx * dx + dy * dy);
+            // console.log("----------------");
+            // console.log("this.x:", this.x, "p5x", p5js.mouseX, p5js.mouseX * PRMS.scale_factor);
+            // console.log("this.y:", this.x, "p5y", p5js.mouseY, p5js.mouseX * PRMS.scale_factor);
+            // console.log("dx:", dx, "dy:", dy, "distance:", distance);
+            this.is_moving = distance < this.diameter / 2;
+            if (this.is_moving && PRMS.hide_cursor) {
+                p5js.noCursor();
+                document.body.style.cursor = 'none';
+            }
+        }
+        if (!this.is_moving) return;
+
+        // ball is moving, only interesred in x-movements
+        this.y -= this.speed;
+        
+        let move_x = p5js.movedX;
+        
+        if (this.noise_type === "reversal") {
+            let prob = PRMS.noise_reversal_prob[this.noise_level];
+            if (Math.random() < prob) {
+                move_x *= -1;
+            }
+        } else if (this.noise_type === "gaussian") {
+            let sd = PRMS.noise_gaussian_sd[this.noise_level];
+            move_x += p5js.randomGaussian(0, sd);
+        }
+        
+        this.x += move_x;
+
+        // wait till path start
+        if (this.y > CANVAS_SIZE[1] - PRMS.path_start) return;
+
+        if (this.step >= this.length) {
+            this.is_moving = false;
+            this.is_complete = true;
+            return;
+        }
+
+        this.step += 1;
+        this.x_path.push(this.x);
+        this.y_path.push(this.y);
+    }
+
+    draw_ball() {
+        p5js.stroke(1);
+        p5js.fill(0, 0, 0);
+        p5js.circle(this.x, this.y, this.diameter);
+    }
+
+    draw_ball_path() {
+        p5js.strokeWeight(2);
+        p5js.stroke(0, 0, 0);
+        for (let x = 1; x < this.x_path.length; x++) {
+            p5js.line(this.x_path[x - 1], this.y_path[x - 1], this.x_path[x], this.y_path[x]);
+        }
+    }
+}
+
+const PATH = new Path(1);
+const BALL = new Ball(1);
+
+function draw_trial() {
+    p5js.background(...PRMS.colours.background);
+    BALL.move();
+    PATH.draw_target_path();
+    BALL.draw_ball();
+    PATH.calculate_distance(BALL.x, BALL.step, PRMS.distance_criterion);
+    if (PRMS.show_ball_path) BALL.draw_ball_path();
+
+    // Piloting text
+    if (PRMS.show_condition_info) {
+        p5js.noStroke();
+        p5js.fill(0);
+        p5js.textAlign(p5js.LEFT);
+        p5js.textSize(20);
+        p5js.text(`Noise Type: ${BALL.noise_type} | Noise Level: ${BALL.noise_level}`, 20, 30);
+    }
+
+    if (BALL.is_complete && !BALL.trial_finished) {
+        if (PRMS.hide_cursor) {
+            p5js.cursor();
+            document.body.style.cursor = 'auto';
+        }
+        BALL.trial_finished = true;
+        jsPsych.finishTrial();
+    }
+}
+
+function add_data() {
+    let on_path_count = PATH.on_path.filter(x => x === 1).length;
+    let total_count = PATH.on_path.filter(x => x === 1 || x === -1).length;
+    let performance = total_count > 0 ? Math.round((on_path_count / total_count) * 100) : 0;
+    PRMS.last_performance = performance;
+
+    jsPsych.data.addDataToLastTrial({
+        date: Date(),
+        block: PRMS.count_block,
+        trial: PRMS.count_trial,
+        path_x: PATH.x,
+        path_y: PATH.y,
+        ball_x: BALL.x_path,
+        ball_y: BALL.y_path,
+        on_path: PATH.on_path,
+        performance: performance,
+    });
+}
+
+const TRIAL = {
+    type: jsPsychP5JSKeyboardResponse,
+    canvas_size: CANVAS_SIZE,
+    canvas_border: CANVAS_BORDER,
+    canvas_background: CANVAS_COLOUR,
+    draw: function () {
+        return draw_trial;
+    },
+    response_ends_trial: false,
+    choices: "NO_KEYS",
+    data: {
+        stim_type: "ftp",
+        noise_level: jsPsych.timelineVariable("noise_level"),
+    },
+    on_start: function (trial) {
+        let speed_diff = "standard";
+        let path_diff = "standard";
+        let noise_level = jsPsych.evaluateTimelineVariable("noise_level");
+
+        PATH.reset(PRMS.speed_difficulty[speed_diff]);
+        PATH.perlin_noise_coordinates(
+            PRMS.path_difficulty[path_diff],
+            PRMS.speed_difficulty[speed_diff],
+        );
+
+        BALL.reset(PATH.y.length);
+        BALL.set_speed(PRMS.speed_difficulty[speed_diff]);
+        BALL.set_x_position(PATH.x[0]);
+        BALL.noise_type = PRMS.noise_type;
+        BALL.noise_level = noise_level;
+        BALL.trial_finished = false;
+    },
+    on_finish: function () {
+        add_data();
+        PRMS.count_trial += 1;
+    },
+};
+
+const TRIAL_PERFORMANCE_FEEDBACK = {
+    type: jsPsychHtmlKeyboardResponseCanvas,
+    canvas_colour: CANVAS_COLOUR,
+    canvas_size: CANVAS_SIZE,
+    canvas_border: CANVAS_BORDER,
+    response_ends_trial: false,
+    trial_duration: 1500,
+    stimulus: function() {
+        return generate_formatted_html({
+            text: `Time on Path: ${PRMS.last_performance}%`,
+            align: "center",
+            colour: "black",
+            fontsize: 50,
+            bold: true,
+        });
+    }
+};
+
+const TRIAL_PERFORMANCE_FEEDBACK_NODE = {
+    timeline: [TRIAL_PERFORMANCE_FEEDBACK],
+    conditional_function: function() {
+        return PRMS.show_percentage_path;
+    }
+};
+
+const ITI = {
+    type: jsPsychHtmlKeyboardResponseCanvas,
+    canvas_colour: CANVAS_COLOUR,
+    canvas_size: CANVAS_SIZE,
+    canvas_border: CANVAS_BORDER,
+    stimulus: "",
+    response_ends_trial: false,
+    trial_duration: PRMS.iti,
+};
+
+const BLOCK_START = {
+    type: jsPsychHtmlKeyboardResponseCanvas,
+    canvas_colour: CANVAS_COLOUR,
+    canvas_size: CANVAS_SIZE,
+    canvas_border: CANVAS_BORDER,
+    stimulus: "",
+    response_ends_trial: true,
+    on_start: function (trial) {
+        trial.stimulus = generate_formatted_html({
+            text: `Start Block ${PRMS.count_block} von ${PRMS.n_blocks}:<br><br> 
+                   Click the left mouse button inside the black ball to start the trial.<br><br>
+                   Control the ball using the mouse by moving left and right.<br><br>
+                   Try to follow the path!<br><br><br>
+                   Drücke eine beliebige Taste, um fortzufahren`,
+            align: "left",
+            colour: "black",
+            fontsize: 30,
+            bold: true,
+        });
+    },
+};
+
+const BLOCK_END = {
+    type: jsPsychHtmlKeyboardResponseCanvas,
+    canvas_colour: CANVAS_COLOUR,
+    canvas_size: CANVAS_SIZE,
+    canvas_border: CANVAS_BORDER,
+    stimulus: "",
+    response_ends_trial: true,
+    on_start: function (trial) {
+        trial.stimulus = generate_formatted_html({
+            text: `Ende Block ${PRMS.count_block} von ${PRMS.n_blocks}:<br><br> Drücke eine beliebige Taste, um fortzufahren`,
+            align: "center",
+            colour: "black",
+            fontsize: 30,
+            bold: true,
+        });
+    },
+    on_finish: function () {
+        PRMS.count_trial = 1;
+        PRMS.count_block += 1;
+    },
+};
+
+function generate_likert_labels(min, max, left_label, right_label) {
+    let labels = [];
+    for (let i = min; i <= max; i++) {
+        if (i === min) labels.push(`${i}<br>${left_label}`);
+        else if (i === max) labels.push(`${i}<br>${right_label}`);
+        else labels.push(`${i}`);
+    }
+    return labels;
+}
+
+const SURVEY_LIKERT = {
+    type: jsPsychSurveyLikert,
+    scale_width: CANVAS_SIZE[0] * 0.9,
+    questions: [
+        {
+            prompt: `<span style="font-weight: bold; font: 36px Arial;">${PRMS.survey_question_1}</span>`,
+            name: "question_1",
+            labels: generate_likert_labels(PRMS.survey_scale[0], PRMS.survey_scale[1], PRMS.survey_anchors[0], PRMS.survey_anchors[1]),
+        },
+        {
+            prompt: `<span style="font-weight: bold; font: 36px Arial;">${PRMS.survey_question_2}</span>`,
+            name: "question_2",
+            labels: generate_likert_labels(PRMS.survey_scale[0], PRMS.survey_scale[1], PRMS.survey_anchors[0], PRMS.survey_anchors[1]),
+        },
+    ],
+};
+
+const SURVEY_SLIDER = {
+    type: jsPsychSurveyHtmlForm,
+    html: `
+        <div style="margin-bottom: 40px; text-align: center;">
+            <p><span style="font-weight: bold; font: 36px Arial;">${PRMS.survey_question_1}</span></p>
+            <input type="range" name="question_1" min="${PRMS.survey_scale[0]}" max="${PRMS.survey_scale[1]}" value="${Math.round((PRMS.survey_scale[0] + PRMS.survey_scale[1]) / 2)}" style="width: 50%;">
+            <div style="display: flex; justify-content: space-between; width: 50%; margin: 0 auto; font: 24px Arial;">
+                <span>${PRMS.survey_scale[0]}<br>${PRMS.survey_anchors[0]}</span><span>${PRMS.survey_scale[1]}<br>${PRMS.survey_anchors[1]}</span>
+            </div>
+        </div>
+        <div style="margin-bottom: 40px; text-align: center;">
+            <p><span style="font-weight: bold; font: 36px Arial;">${PRMS.survey_question_2}</span></p>
+            <input type="range" name="question_2" min="${PRMS.survey_scale[0]}" max="${PRMS.survey_scale[1]}" value="${Math.round((PRMS.survey_scale[0] + PRMS.survey_scale[1]) / 2)}" style="width: 50%;">
+            <div style="display: flex; justify-content: space-between; width: 50%; margin: 0 auto; font: 24px Arial;">
+                <span>${PRMS.survey_scale[0]}<br>${PRMS.survey_anchors[0]}</span><span>${PRMS.survey_scale[1]}<br>${PRMS.survey_anchors[1]}</span>
+            </div>
+        </div>
+    `,
+    on_finish: function(data) {
+        data.response.question_1 = Number(data.response.question_1);
+        data.response.question_2 = Number(data.response.question_2);
+    }
+};
+
+const SURVEY = {
+    timeline: [
+        {
+            timeline: [SURVEY_LIKERT],
+            conditional_function: function() { return PRMS.survey_type === "likert"; }
+        },
+        {
+            timeline: [SURVEY_SLIDER],
+            conditional_function: function() { return PRMS.survey_type === "slider"; }
+        }
+    ]
+};
+
+const TRIAL_TABLE = [
+    { noise_level: "low" },
+    { noise_level: "med" },
+    { noise_level: "high" },
+];
+
+const TRIAL_TIMELINE = {
+    timeline: [TRIAL, TRIAL_PERFORMANCE_FEEDBACK_NODE, SURVEY, ITI],
+    timeline_variables: TRIAL_TABLE,
+    sample: {
+        type: "fixed-repetitions",
+        size: PRMS.n_trials / 3,
+    }
+};
+
+////////////////////////////////////////////////////////////////////////
+//                             VP Stunden                             //
+////////////////////////////////////////////////////////////////////////
+const END_SCREEN = {
+    type: jsPsychHtmlKeyboardResponseCanvas,
+    canvas_colour: CANVAS_COLOUR,
+    canvas_size: CANVAS_SIZE,
+    canvas_border: CANVAS_BORDER,
+    response_ends_trial: true,
+    choices: [" "],
+    stimulus: generate_formatted_html({
+        text: `Dieser Teil des Experiments ist jetzt beendet.<br><br>
+             Nun folgen Informationen zur Versuchspersonenstunde auf SONA.
+             Drücke eine beliebige Taste, um die Weiterleitung zu SONA zu starten.`,
+        fontsize: 28,
+        lineheight: 1.5,
+        bold: true,
+        align: "left",
+    }),
+};
+
+
+
+////////////////////////////////////////////////////////////////////////
+//                              Save                                  //
+////////////////////////////////////////////////////////////////////////
+const DIR_NAME = get_dir_name();
+const EXP_NAME = get_file_name();
+const VP_NUM = get_time();
+
+function save() {
+    jsPsych.data.addProperties({ vpNum: VP_NUM });
+
+    const data_fn = `${DIR_NAME}data/${EXP_NAME}_${VP_NUM}`;
+    //saveData("/Common/write_data.php", data_fn, { stim_type: "ftp" }, (filetype = "json"));
+    save_data_local(data_fn, { stim_type: "ftp" }, (filetype = "json"));
+}
+
+const SAVE_DATA = {
+    type: jsPsychCallFunction,
+    func: save,
+    post_trial_gap: 1000,
+};
+
+////////////////////////////////////////////////////////////////////////
+//                    Generate and run experiment                     //
+////////////////////////////////////////////////////////////////////////
+function genExpSeq() {
+    "use strict";
+
+    let exp = [];
+
+    exp.push(fullscreen(true));
+    exp.push(browser_check([CANVAS_SIZE[1], CANVAS_SIZE[0]]));
+    exp.push(resize_browser());
+    exp.push(SCALE_FACTOR);
+    exp.push(welcome_message());
+    // exp.push(vp_info_form("/Common8+/vpInfoForm_de.html"));
+    exp.push(WELCOME_INSTRUCTIONS);
+
+    for (let blk = 0; blk < PRMS.n_blocks; blk += 1) {
+        exp.push(BLOCK_START);
+        exp.push(TRIAL_TIMELINE);
+        exp.push(BLOCK_END);
+    }
+
+    // save data
+    exp.push(SAVE_DATA);
+
+    // debrief
+    exp.push(END_SCREEN);
+    exp.push(end_message());
+    exp.push(fullscreen(false));
+
+    return exp;
+}
+const EXP = genExpSeq();
+
+jsPsych.run(EXP);
